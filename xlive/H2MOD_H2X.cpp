5a0ae6910ba86a7f8d4d65a0f886ea4d487caa3a
UF ShadowTeddy <21265755+ShadowTeddy@users.noreply.github.com>
2017-11-05 23:47:10 +0000

    H2X-Rebalance final additions
    This, hopefully should be the final weapon definitions for the H2X rebalance for H2V. 
    
    Changes:
    The Rocket Launcher, Plasma Rifle & Brute Plasma Rifle have been added. 
    Beam Rifle has been changed from 0.25875 to 0.28
    Brute Shot has been changed from 0.39 to 0.40

--- xlive/H2MOD_H2X.cpp
+++ xlive/H2MOD_H2X.cpp
@@ -14,10 +14,13 @@ void H2X::Initialize()
 
 	*(float*)(FloatOffsets + 0xA49A7C) = 0.295f; /*H2X BR fire recovery time*/
 	*(float*)(FloatOffsets + 0xB7A330) = 0.535f; /*H2X Sniper Rifle fire recovery time*/
-	*(float*)(FloatOffsets + 0xCE049C) = 0.25875f; /*H2X Beam Rifle fire recovery time*/
-	*(float*)(FloatOffsets + 0xA7C3D4) = 0.20f; /*H2X Carbine fire recovery time*/
+	*(float*)(FloatOffsets + 0xCE049C) = 0.28f; /*H2X Beam Rifle fire recovery time*/
+	*(float*)(FloatOffsets + 0xA7C3D4) = 0.20f; /*H2X Carbine fire recovery time. Thanks RE4thewin for input on this.*/
 	*(float*)(FloatOffsets + 0xAF1DA4) = 1.035f; /*H2X Shotgun fire recovery time*/
 	*(float*)(FloatOffsets + 0x96EC34) = 0.13f; /*H2X Magnum fire recovery time*/
-	*(float*)(FloatOffsets + 0xC0EABC) = 0.39f; /*H2X Brute Shot fire recovery time*/
+	*(float*)(FloatOffsets + 0xC0EABC) = 0.40f; /*H2X Brute Shot fire recovery time*/
 	*(float*)(FloatOffsets + 0xA03250) = 0.11f; /*H2X Plasma Pistol fire recovery time*/
+	*(float*)(FloatOffsets + 0xBAEAF0) = 0.85f; /*H2X Rocket Launcher fire recovery time*/
+	*(float*)(FloatOffsets + 0xAAE544) = 8.5f; /*H2X Plasma Rifle fire recovery time*/
+	*(float*)(FloatOffsets + 0xD0F960) = 10.0f; /*H2X Brute Plasma Rifle fire recovery time*/
 }
