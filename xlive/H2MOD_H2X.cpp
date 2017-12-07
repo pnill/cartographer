e3080e9c60f5759ff2e8c9648b472e9eedfe2862
NukeULater <cosminavieritei@gmail.com>
2017-12-07 11:52:44 +0200

    Cleaned H2X code
    *Check if the arg is number in maxplayers command using yoshi's function

--- xlive/H2MOD_H2X.cpp
+++ xlive/H2MOD_H2X.cpp
@@ -3,9 +3,7 @@
 #include "xliveless.h"
 #include "H2MOD.h"
 
-bool H2X_Disable = false;
-
-void H2X::H2X_Initialize()
+void H2X_Initialize(bool init)
 {
 	int offset = 0x47CD54;
 	if (h2mod->Server)
@@ -13,36 +11,16 @@ void H2X::H2X_Initialize()
 	TRACE_GAME("[h2mod] H2X is being run on client game");
 	DWORD FloatOffsets = *(DWORD*)((char*)h2mod->GetBase() + offset);
 
-	*(float*)(FloatOffsets + 0xA49A7C) = 0.295f; /*H2X BR fire recovery time*/
-	*(float*)(FloatOffsets + 0xB7A330) = 0.535f; /*H2X Sniper Rifle fire recovery time*/
-	*(float*)(FloatOffsets + 0xCE049C) = 0.25875f; /*H2X Beam Rifle fire recovery time*/
-	*(float*)(FloatOffsets + 0xA7C3D4) = 0.20f; /*H2X Carbine fire recovery time*/
-	*(float*)(FloatOffsets + 0xAF1DA4) = 1.035f; /*H2X Shotgun fire recovery time*/
-	*(float*)(FloatOffsets + 0x96EC34) = 0.13f; /*H2X Magnum fire recovery time*/
-	*(float*)(FloatOffsets + 0xC0EABC) = 0.39f; /*H2X Brute Shot fire recovery time*/
-	*(float*)(FloatOffsets + 0xA03250) = 0.11f; /*H2X Plasma Pistol fire recovery time*/
-	*(float*)(FloatOffsets + 0xBDBF50) = 0.85f; /*H2X Rocket Launcher fire recovery time*/
-	*(float*)(FloatOffsets + 0xAAE544) = 8.5f; /*H2X Plasma Rifle rounds per second max*/
-	*(float*)(FloatOffsets + 0xD0F960) = 10.0f; /*H2X Brute Plasma Rifle rounds per second max*/
+	*(float*)(FloatOffsets + 0xA49A7C) = init ? 0.295f : 0.26f; /*H2X/H2V BR fire recovery time*/
+	*(float*)(FloatOffsets + 0xB7A330) = init ? 0.535f : 0.5f; /*H2X/H2V Sniper Rifle fire recovery time*/
+	*(float*)(FloatOffsets + 0xCE049C) = init ? 0.25875f : 0.25f; /*H2X/H2V Beam Rifle fire recovery time*/
+	*(float*)(FloatOffsets + 0xA7C3D4) = init ? 0.20f : 0.14f; /*H2X/H2V Carbine fire recovery time*/
+	*(float*)(FloatOffsets + 0xAF1DA4) = init ? 1.035f : 1.0f; /*H2X/H2V Shotgun fire recovery time*/
+	*(float*)(FloatOffsets + 0x96EC34) = init ? 0.13f : 0.1f; /*H2X/H2V Magnum fire recovery time*/
+	*(float*)(FloatOffsets + 0xC0EABC) = init ? 0.39f : 0.3f; /*H2X/H2V Brute Shot fire recovery time*/
+	*(float*)(FloatOffsets + 0xA03250) = init ? 0.11f : 0.05f; /*H2X/H2V Plasma Pistol fire recovery time*/
+	*(float*)(FloatOffsets + 0xBDBF50) = init ? 0.85f : 0.8f; /*H2X/H2V Rocket Launcher fire recovery time*/
+	*(float*)(FloatOffsets + 0xAAE544) = init ? 8.5f : 9.0f; /*H2X/H2V Plasma Rifle rounds per second max*/
+	*(float*)(FloatOffsets + 0xD0F960) = init ? 10.0f : 11.0f ; /*H2X/H2V Brute Plasma Rifle rounds per second max*/
 }
 	
-void H2X::H2X_Deinitialize()
-{
-	int offset = 0x47CD54;
-	if (h2mod->Server)
-		offset = 0x4A29BC;
-	TRACE_GAME("[h2mod] H2X is not running on client game");
-	DWORD FloatOffsets = *(DWORD*)((char*)h2mod->GetBase() + offset);
-	
-	*(float*)(FloatOffsets + 0xA49A7C) = 0.26f; /*H2V BR fire recovery time*/
-	*(float*)(FloatOffsets + 0xB7A330) = 0.5f; /*H2V Sniper Rifle fire recovery time*/
-	*(float*)(FloatOffsets + 0xCE049C) = 0.25f; /*H2V Beam Rifle fire recovery time*/
-	*(float*)(FloatOffsets + 0xA7C3D4) = 0.14f; /*H2V Carbine fire recovery time*/
-	*(float*)(FloatOffsets + 0xAF1DA4) = 1.0f; /*H2V Shotgun fire recovery time*/
-	*(float*)(FloatOffsets + 0x96EC34) = 0.1f; /*H2V Magnum fire recovery time*/
-	*(float*)(FloatOffsets + 0xC0EABC) = 0.3f; /*H2V Brute Shot fire recovery time*/
-	*(float*)(FloatOffsets + 0xA03250) = 0.05f; /*H2V Plasma Pistol fire recovery time*/
-	*(float*)(FloatOffsets + 0xBDBF50) = 0.8f; /*H2X Rocket Launcher fire recovery time*/
-	*(float*)(FloatOffsets + 0xAAE544) = 9.0f; /*H2X Plasma Rifle rounds per second max*/
-	*(float*)(FloatOffsets + 0xD0F960) = 11.0f; /*H2X Brute Plasma Rifle rounds per second max*/
-}
--- xlive/H2MOD_H2X.h
+++ xlive/H2MOD_H2X.h
@@ -1,12 +1,4 @@
-#ifndef H2MOD_H2X
-#define H2MOD_H2X
+#pragma once
 
-class H2X
-{
-public:
+	void H2X_Initialize(bool init);
 
-	void H2X_Initialize();
-	void H2X_Deinitialize();
-};
-
-#endif
