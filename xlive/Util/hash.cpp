887e09c8d9f6446b8703f5bcc893d1f395ae1f59
num0005 <double.null@outlook.com>
2018-09-09 14:47:30 +0100

    Extend hash helper functions.
    Add support for wide paths and only hashing part of the file.

--- xlive/Util/hash.cpp
+++ xlive/Util/hash.cpp
@@ -3,10 +3,11 @@
 #include <sstream>
 #include <iomanip>
 #include "hash.h"
+#include <codecvt>
 
-bool hash_open_file(const char *file_name, HANDLE &file)
+bool hash_open_file(const wchar_t *file_name, HANDLE &file)
 {
-	file = CreateFileA(file_name, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+	file = CreateFileW(file_name, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_SEQUENTIAL_SCAN, NULL);
 	return LOG_CHECK(file != INVALID_HANDLE_VALUE);
 }
 
@@ -21,11 +22,15 @@ bool hash_get_crypto_provider(DWORD type, HCRYPTPROV *provider)
 
 #define file_chunk_size 1024
 
-bool hash_do_file_hashing(HANDLE file, HCRYPTHASH hash, DWORD flags)
+bool hash_do_file_hashing(HANDLE file, HCRYPTHASH hash, DWORD flags, long long len)
 {
+	if (len == 0) {
+		len = LONG_MAX;
+	}
 	BYTE file_chunk[file_chunk_size];
+	DWORD len_to_read = min(file_chunk_size, len);
 	DWORD bytes_read = 0;
-	while (LOG_CHECK(ReadFile(file, file_chunk, file_chunk_size,
+	while (LOG_CHECK(ReadFile(file, file_chunk, len_to_read,
 		&bytes_read, NULL)))
 	{
 		if (bytes_read == 0)
@@ -37,6 +42,10 @@ bool hash_do_file_hashing(HANDLE file, HCRYPTHASH hash, DWORD flags)
 		{
 			return false;
 		}
+		len = len - bytes_read;
+		if (len <= 0)
+			return true;
+		len_to_read = min(file_chunk_size, len);
 	}
 	return true;
 }
@@ -54,7 +63,7 @@ std::string hashes::as_hex_string(BYTE *data, size_t data_len)
 
 #define md5_len 16
 
-bool hashes::calc_file_md5(const std::string &filename, BYTE *checksum, size_t &checksum_len)
+bool hashes::calc_file_md5(const std::wstring &filename, BYTE *checksum, size_t &checksum_len, long long len_read)
 {
 	HANDLE file;
 	if (hash_open_file(filename.c_str(), file))
@@ -65,7 +74,7 @@ bool hashes::calc_file_md5(const std::string &filename, BYTE *checksum, size_t &
 			HCRYPTHASH hash = NULL;
 			if (LOG_CHECK(CryptCreateHash(crypto_provider, CALG_MD5, 0, 0, &hash)))
 			{
-				if (hash_do_file_hashing(file, hash, 0))
+				if (hash_do_file_hashing(file, hash, 0, len_read))
 				{
 					DWORD hash_len = min(checksum_len, md5_len);
 					if (LOG_CHECK(CryptGetHashParam(hash, HP_HASHVAL, checksum, &hash_len, 0)))
@@ -89,14 +98,24 @@ bool hashes::calc_file_md5(const std::string &filename, BYTE *checksum, size_t &
 }
 
 
-bool hashes::calc_file_md5(const std::string &filename, std::string &checksum_out)
+bool hashes::calc_file_md5(const std::wstring &filename, std::string &checksum_out, long long len_read)
 {
 	BYTE hash_data[md5_len];
 	size_t hash_len = ARRAYSIZE(hash_data);
-	if (calc_file_md5(filename, hash_data, hash_len))
+	if (calc_file_md5(filename, hash_data, hash_len, len_read))
 	{
 		checksum_out = hashes::as_hex_string(hash_data, hash_len);
 		return true;
 	}
 	return false;
+}
+
+static std::wstring_convert<std::codecvt_utf8<wchar_t>> wstring_to_string;
+bool hashes::calc_file_md5(const std::string &filename, BYTE *checksum, size_t &checksum_len, long long len_read)
+{
+	return hashes::calc_file_md5(wstring_to_string.from_bytes(filename), checksum, checksum_len, len_read);
+}
+bool hashes::calc_file_md5(const std::string &filename, std::string &checksum_out, long long len_read)
+{
+	return hashes::calc_file_md5(wstring_to_string.from_bytes(filename), checksum_out, len_read);
 }
--- xlive/Util/hash.h
+++ xlive/Util/hash.h
@@ -5,6 +5,9 @@ namespace hashes
 {
 	std::string as_hex_string(BYTE *data, size_t data_len);
 
-	bool calc_file_md5(const std::string &filename, BYTE *checksum, size_t &checksum_len);
-	bool calc_file_md5(const std::string &filename, std::string &checksum_out);
+	bool calc_file_md5(const std::string &filename, BYTE *checksum, size_t &checksum_len, long long count = 0);
+	bool calc_file_md5(const std::string &filename, std::string &checksum_out, long long count = 0);
+
+	bool calc_file_md5(const std::wstring &filename, BYTE *checksum, size_t &checksum_len, long long count = 0);
+	bool calc_file_md5(const std::wstring &filename, std::string &checksum_out, long long count = 0);
 };
