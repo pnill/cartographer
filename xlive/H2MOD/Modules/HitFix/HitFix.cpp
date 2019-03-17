73cd85da65e223cfd46190dc5abc49a82eaf2cb7
NukeULater <nukeulater@gmail.com>
2019-03-17 14:15:31 +0200

    Re-enable hitfix on sniper and gauss.

--- xlive/H2MOD/Modules/HitFix/HitFix.cpp
+++ xlive/H2MOD/Modules/HitFix/HitFix.cpp
@@ -15,14 +15,14 @@ void HitFix::Initialize()
 	*(float*)(AddressOffset + 0xA4EC8C) = 800.0f;	//battle_rifle_bullet.proj Final Velocity 400
 	*(float*)(AddressOffset + 0xA84DD4) = 800.0f;   //carbine_projectile       Initial Velocity 400
 	*(float*)(AddressOffset + 0xA84DD8) = 800.0f;   //carbine_projectile       Final Velocity 400
-	//*(float*)(AddressOffset + 0xB7F914) = 2400.0f;	//sniper_bullet.proj Initial Velocity 1200
-	//*(float*)(AddressOffset + 0xB7F918) = 2400.0f;	//sniper_bullet.proj Final Velocity 1200
+	*(float*)(AddressOffset + 0xB7F914) = 4000.0f;	//sniper_bullet.proj Initial Velocity 1200
+	*(float*)(AddressOffset + 0xB7F918) = 4000.0f;	//sniper_bullet.proj Final Velocity 1200
 	
 	//FIXME COOP will break because of one of these tags not existing.
-	//*(float*)(AddressOffset + 0xCE4598) = 2400.0f;	//beam_rifle_beam.proj Initial Velocity 1200
-	//*(float*)(AddressOffset + 0xCE459C) = 2400.0f;	//beam_rifle_beam.proj Final Velocity 1200
-	//*(float*)(AddressOffset + 0x81113C) = 180.0f;	//gauss_turret.proj Initial Velocity def 90
-	//*(float*)(AddressOffset + 0x811140) = 180.0f;	//gauss_turret.proj Final Velocity def 90
+	*(float*)(AddressOffset + 0xCE4598) = 4000.0f;	//beam_rifle_beam.proj Initial Velocity 1200
+	*(float*)(AddressOffset + 0xCE459C) = 4000.0f;	//beam_rifle_beam.proj Final Velocity 1200
+	*(float*)(AddressOffset + 0x81113C) = 200.0f;	//gauss_turret.proj Initial Velocity def 90
+	*(float*)(AddressOffset + 0x811140) = 200.0f;	//gauss_turret.proj Final Velocity def 90
 	*(float*)(AddressOffset + 0x97A194) = 800.0f;	//magnum_bullet.proj initial def 400
 	*(float*)(AddressOffset + 0x97A198) = 800.0f;	//magnum_bullet.proj final def 400
 	*(float*)(AddressOffset + 0x7E7E20) = 1600.0f;	//bullet.proj (chaingun) initial def 800
