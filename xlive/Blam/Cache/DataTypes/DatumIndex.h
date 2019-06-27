1a0c209b94765645c2a52c7c251204cd4c361013
num0005 <double.null@outlook.com>
2019-06-27 23:45:45 +0100

    Put DatumIndex back in that namespace.

--- xlive/Blam/Cache/DataTypes/DatumIndex.h
+++ xlive/Blam/Cache/DataTypes/DatumIndex.h
@@ -5,72 +5,75 @@ namespace Blam
 {
 	namespace Cache
 	{
-		/*********************************************************************
-		* Blam::Cache::DatumIndex
-		* 4 BYTE Tag Structure for any TagIndices of Halo 2 CacheFiles
-		**********************************************************************/
-		struct DatumIndex
+		namespace DataTypes
 		{
-			//Default Constructor
-			DatumIndex()
+			/*********************************************************************
+			* Blam::Cache::DataTypes::DatumIndex
+			* 4 BYTE Tag Structure for any TagIndices of Halo 2 CacheFiles
+			**********************************************************************/
+			struct DatumIndex
 			{
-				this->Index = -1;
-				this->Salt = -1;
-			}
-			//Copy Constructor
-			DatumIndex(INT32 Identifier)
-			{
-				this->data = Identifier;
-			}
+				//Default Constructor
+				DatumIndex()
+				{
+					this->Index = -1;
+					this->Salt = -1;
+				}
+				//Copy Constructor
+				DatumIndex(INT32 Identifier)
+				{
+					this->data = Identifier;
+				}
 
-			constexpr DatumIndex(short _index, short _salt) :
-				Index(_index),
-				Salt(_salt)
-			{
-			}
+				constexpr DatumIndex(short _index, short _salt) :
+					Index(_index),
+					Salt(_salt)
+				{
+				}
 
-			static constexpr INT32 Null = 0xFFFFFFFF;
+				static constexpr INT32 Null = 0xFFFFFFFF;
 
-			//Returns true is DatumIndex is not Invalid
-			inline bool IsNull()
-			{
-				return this->Salt = -1 && this->Index == -1;
-			}
+				//Returns true is DatumIndex is not Invalid
+				inline bool IsNull()
+				{
+					return this->Salt = -1 && this->Index == -1;
+				}
 
-			inline unsigned long ToInt()
-			{
-				return data;
-			}
+				inline unsigned long ToInt()
+				{
+					return data;
+				}
 
-			inline signed short ToAbsoluteIndex() { return Index; };
+				inline signed short ToAbsoluteIndex() { return Index; };
 
-			inline bool operator == (const DatumIndex &other)
-			{
-				return this->data == other.data;
-			}
-			inline bool operator != (const DatumIndex &other)
-			{
-				return this->data != other.data;
-			}
+				inline bool operator == (const DatumIndex &other)
+				{
+					return this->data == other.data;
+				}
+				inline bool operator != (const DatumIndex &other)
+				{
+					return this->data != other.data;
+				}
 
-			inline std::string ToString() const
-			{
-				std::stringstream ss;
-				ss << "Index: " << std::hex << Index <<
-					" Salt: " << std::hex << Salt <<
-					" Datum: " << std::hex << data;
-				return ss.str();
-			}
+				inline std::string ToString() const
+				{
+					std::stringstream ss;
+					ss << "Index: " << std::hex << Index <<
+						" Salt: " << std::hex << Salt <<
+						" Datum: " << std::hex << data;
+					return ss.str();
+				}
 
-			union {
-				struct {
-					signed short Index;
-					signed short Salt;
+				union {
+					struct {
+						signed short Index;
+						signed short Salt;
+					};
+					unsigned long data;
 				};
-				unsigned long data;
 			};
-		};
-		static_assert(sizeof(DatumIndex) == 4, "Invalid Size for struct (DatumIndex)");
+			static_assert(sizeof(DatumIndex) == 4, "Invalid Size for struct (DatumIndex)");
+		}
 	}
 }
 			
