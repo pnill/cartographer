331fb59324bba3370d84d587ccf664ea72531d13
Himanshu-01 <kujurhimanshu66@gmail.com>
2019-07-04 23:21:28 +0530

    Add MetaLoader

--- xlive/H2MOD.cpp
+++ xlive/H2MOD.cpp
@@ -17,6 +17,7 @@
 #include "H2MOD\Modules\Networking\CustomPackets\CustomPackets.h"
 #include <Shlwapi.h>
 #include "H2MOD\Tags\global_tags_interface.h"
+#include "MetaLoader\tag_loader.h"
 
 H2MOD *h2mod = new H2MOD();
 GunGame* gunGame = new GunGame();
@@ -1702,6 +1703,9 @@ void H2MOD::ApplyHooks() {
 		PatchCall(GetBase() + 0x00182d6d, GrenadeChainReactIsEngineMPCheck);
 		PatchCall(GetBase() + 0x00092C05, BansheeBombIsEngineMPCheck);
 		PatchCall(GetBase() + 0x0013ff75, FlashlightIsEngineSPCheck);
+
+		Initialise_tag_loader();
+		TagInterface::GlobalTagInterface.Init();
 	}
 	else {
 
@@ -1782,8 +1786,7 @@ void H2MOD::Initialize()
 	LOG_TRACE_GAME("H2MOD - BASE ADDR {:x}", this->Base);
 
 	h2mod->ApplyHooks();
-	tags::apply_patches();
-	TagInterface::GlobalTagInterface.Init();
+	tags::apply_patches();	
 }
 
 void H2MOD::Deinitialize() {
