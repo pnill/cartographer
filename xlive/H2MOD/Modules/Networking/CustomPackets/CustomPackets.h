c335ec9596409e2c243f669530160fc71f172de8
NukeULater <nukeulater@gmail.com>
2020-03-13 10:11:51 +0200

    Align custom map filename struct to 8 bytes

--- xlive/H2MOD/Modules/Networking/CustomPackets/CustomPackets.h
+++ xlive/H2MOD/Modules/Networking/CustomPackets/CustomPackets.h
@@ -119,7 +119,7 @@ static const char* network_message_name[] = {
 	"unit_grenades"
 };
 
-struct s_custom_map_filename
+struct __declspec(align(8)) s_custom_map_filename
 {
 	bool is_custom_map;
 	wchar_t file_name[32];
