6d03b8615a8f97f77c0d300cd2cc8114a7bf46cc
NukeULater <nukeulater@gmail.com>
2020-08-08 20:58:15 +0300

    Updated UncappedFPS.cpp
    more research going on

--- xlive/H2MOD/Modules/MainLoopPatches/UncappedFPS/UncappedFPS.cpp
+++ xlive/H2MOD/Modules/MainLoopPatches/UncappedFPS/UncappedFPS.cpp
@@ -5,6 +5,10 @@
 #include "Util/Hooks/Hook.h"
 
 extern bool b_XboxTick;
+static LARGE_INTEGER frequency;
+static LARGE_INTEGER timeAtStartup;
+
+#define COMPUTE_DELTA_TIME_PATCHES 0
 
 void __cdecl compute_target_tick_count(float a1, float* a2, int* out_target_tick_count)
 {
@@ -12,8 +16,13 @@ void __cdecl compute_target_tick_count(float a1, float* a2, int* out_target_tick
 	auto p_compute_target_tick_count = h2mod->GetAddress<compute_target_tick_count>(0x7C1BF);
 
 	//a1 = 1.f / 60.f;
+
 	p_compute_target_tick_count(a1, a2, out_target_tick_count);
-	LOG_TRACE_GAME("input_float: {}, dt: {}, target_tick_count: {}", a1, *a2, *out_target_tick_count);
+	
+	if (*out_target_tick_count == 0)
+	{
+		LOG_TRACE_GAME("input_float: {}, dt: {}, target_tick_count: {}", a1, *a2, *out_target_tick_count);
+	}
 }
 
 /*
@@ -38,40 +47,51 @@ struct __declspec(align(4)) time_globals
 };
 */
 
-static LARGE_INTEGER PerformanceFrequency;
-static LARGE_INTEGER PerformanceCountAtStartup;
-static LARGE_INTEGER compute_time_delta_last_time;
-
-int WINAPI timeGetTime_hook()
+DWORD dwTimeAtStartup;
+DWORD WINAPI timeGetTime_hook()
 {
-	LARGE_INTEGER PerformanceCount;
-	QueryPerformanceCounter(&PerformanceCount);
-	PerformanceCount.QuadPart = PerformanceCount.QuadPart - PerformanceCountAtStartup.QuadPart;
-	return (unsigned int)(PerformanceCount.QuadPart / (PerformanceFrequency.QuadPart / 1000));
+	return timeGetTime() - dwTimeAtStartup;
+}
+
+static __int64 game_time_replacement;
+__int64 get_game_time()
+{
+	return game_time_replacement;
 }
 
 static LARGE_INTEGER lastTime;
 float __cdecl compute_time_delta(bool use_static_time_increase, float static_time_delta)
 {
-	LARGE_INTEGER frequency;
-	LARGE_INTEGER currentTime;
-	//LARGE_INTEGER* gameTime = h2mod->GetAddress<LARGE_INTEGER*>(0x479E98);
-
 	typedef float(__cdecl* compute_time_delta_def)(bool, float);
 	auto p_compute_time_delta_def = h2mod->GetAddress<compute_time_delta_def>(0x28814);
 
-	QueryPerformanceFrequency(&frequency);
+	LARGE_INTEGER currentTime, elapsedMilliseconds;
 	QueryPerformanceCounter(&currentTime);
-	float time_delta = (double)(currentTime.LowPart - lastTime.LowPart) / (double)frequency.LowPart;
+	elapsedMilliseconds.QuadPart = currentTime.QuadPart - timeAtStartup.QuadPart;
+
+	// We now have the elapsed number of ticks, along with the
+	// number of ticks-per-second. We use these values
+	// to convert to the number of elapsed microseconds.
+	// To guard against loss-of-precision, we convert
+	// to milliseconds *before* dividing by ticks-per-second.
+
+	elapsedMilliseconds.QuadPart *= 1000; // convert to milliseconds
+	elapsedMilliseconds.QuadPart /= frequency.QuadPart;
+	game_time_replacement = elapsedMilliseconds.QuadPart; 
 
 	float ret = p_compute_time_delta_def(use_static_time_increase, static_time_delta);
+
+#if COMPUTE_DELTA_TIME_PATCHES
 	//LOG_TRACE_GAME("compute_time_delta() - time delta: {}", ret);
 
+	QueryPerformanceCounter(&currentTime);
+	ret = (double)(currentTime.LowPart - lastTime.LowPart) / (double)(int)frequency.LowPart;
+	ret = fminf(10.f, ret);
+
 	lastTime = currentTime;
+#endif // COMPUTE_DELTA_TIME_PATCHES
 
-	//*gameTime = currentTime;
-
-	return time_delta;
+	return ret;
 }
 
 // we disable some broken code added by hired gun, that is also disabled while running a cinematic 
@@ -82,8 +102,16 @@ bool __cdecl cinematic_in_progress_hook()
 	typedef bool(__cdecl* cinematic_in_progress)();
 	auto p_cinematic_in_progress = h2mod->GetAddress<cinematic_in_progress>(0x3A938);
 
+	bool unk = *h2mod->GetAddress<bool*>(0x48225B);
+	if (!p_cinematic_in_progress())
+		unk = false;
+
+	*h2mod->GetAddress<bool*>(0x48225B) = unk;
+#if COMPUTE_DELTA_TIME_PATCHES
+	return true;
+#else
 	return p_cinematic_in_progress() || call_is_game_minimized() || call_get_game_tick_rate() < 60 || b_XboxTick;
-	//return true;
+#endif // COMPUTE_DELTA_TIME_PATCHES
 }
 
 bool __cdecl should_limit_framerate()
@@ -97,8 +125,12 @@ void UncappedFPS::ApplyPatches()
 	{
 		//NopFill(h2mod->GetAddress(0x2728E7), 5);
 
-		//PatchCall(h2mod->GetAddress(0x39BE3), compute_time_delta);
-		//PatchCall(h2mod->GetAddress(0x39D04), compute_target_tick_count);
+		PatchCall(h2mod->GetAddress(0x39BE3), compute_time_delta);
+		PatchCall(h2mod->GetAddress(0x39C0D), compute_time_delta);
+
+#if COMPUTE_DELTA_TIME_PATCHES
+		PatchCall(h2mod->GetAddress(0x39D04), compute_target_tick_count);
+#endif // COMPUTE_DELTA_TIME_PATCHES
 
 		//NopFill(h2mod->GetAddress(0x39BDA), 2);
 		//NopFill(h2mod->GetAddress(0x39DF0), 8);
@@ -108,12 +140,15 @@ void UncappedFPS::ApplyPatches()
 		PatchCall(h2mod->GetAddress(0x288B5), should_limit_framerate);
 
 		PatchCall(h2mod->GetAddress(0x39A2A), cinematic_in_progress_hook);
+
+		WriteJmpTo(h2mod->GetAddress(0x1B3C5C), get_game_time);
 	}
 
-	//PatchWinAPICall(h2mod->GetAddress(0x37E51, 0x2B4CE), timeGetTime_hook);
-
+	QueryPerformanceFrequency(&frequency);
+	QueryPerformanceCounter(&timeAtStartup);
 	QueryPerformanceCounter(&lastTime);
-	QueryPerformanceFrequency(&PerformanceFrequency);
-	QueryPerformanceCounter(&PerformanceCountAtStartup);
+
+	//dwTimeAtStartup = timeGetTime();
+	//PatchWinAPICall(h2mod->GetAddress(0x37E51, 0x2B4CE), timeGetTime_hook);
 }
 
