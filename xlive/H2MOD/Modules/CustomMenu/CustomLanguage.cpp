93c71faa68696be565c8cbdc86aba5fee64f6a3a
Kantanomo <>
2020-09-28 17:01:48 -0500

    Adding the label for xlive_task

--- xlive/H2MOD/Modules/CustomMenu/CustomLanguage.cpp
+++ xlive/H2MOD/Modules/CustomMenu/CustomLanguage.cpp
@@ -670,6 +670,7 @@ static void overrideCoreH2Labels() {
 		add_custom_label(lang, 0x000003fd, 0x09000284, "ONLINE");
 		add_custom_label(lang, 0x000003fd, 0x05000986, "CARTOGRAPHER");
 		add_custom_label(lang, 0x00000718, 0x0E0005D9, "Extra Game Settings...");
+		add_custom_label(lang, 0x00000937, 0x0700064a, "Signing into Project Cartographer, please wait...");
 	}
 } 
 
