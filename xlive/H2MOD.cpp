12143a3be4a9683a342071d271f4b08d6f609baf
Kantanomo <mhatto00@gmail.com>
2020-12-10 23:50:47 -0600

    I'm an idiot

--- xlive/H2MOD.cpp
+++ xlive/H2MOD.cpp
@@ -904,32 +904,34 @@ bool __cdecl OnMapLoad(Blam::EngineDefinitions::game_engine_settings* engine_set
 			}
 		}
 		//Christmas hat importing move later, auto disables after january.
-		std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-		time_t tt = std::chrono::system_clock::to_time_t(now);
-		tm utc_tm = *gmtime(&tt);
-		if (utc_tm.tm_mon == 11 || utc_tm.tm_mon == 0) {
-			tag_loader::Load_tag(0xE19B001D, true, "christmas_hat_map");
-			tag_loader::Load_tag(0xE1BF0024, true, "christmas_hat_map");
-			tag_loader::Push_Back();
-			//auto scen = tags::get_tag<blam_tag::tag_group_type::scenery, s_scenery_group_definition>(datum(_INJECTED_TAG_START_));
-			auto hlmt_chief_datum = tags::find_tag(blam_tag::tag_group_type::model, "objects\\characters\\masterchief\\masterchief_mp");
-			if (hlmt_chief_datum != datum::Null) {
-				auto hlmt_chief = tags::get_tag<blam_tag::tag_group_type::model, s_model_group_definition>(hlmt_chief_datum);
-				auto b = hlmt_chief->variants[0];
-				auto a = MetaExtender::add_tag_block2<s_model_group_definition::s_variants_block::s_objects_block>((unsigned long)std::addressof(b->objects));
-				a->parent_marker = string_id(184552154);
-				a->child_object.TagGroup = blam_tag::tag_group_type::scenery;
-				a->child_object.TagIndex = tag_loader::ResolveNewDatum(0xE19B001D);
-			}
-			auto hlmt_elite_datum = tags::find_tag(blam_tag::tag_group_type::model, "objects\\characters\\elite\\elite_mp");
-			if (hlmt_elite_datum != datum::Null)
-			{
-				auto hlmt_eliete = tags::get_tag<blam_tag::tag_group_type::model, s_model_group_definition>(hlmt_elite_datum);
-				auto b = hlmt_eliete->variants[0];
-				auto a = MetaExtender::add_tag_block2<s_model_group_definition::s_variants_block::s_objects_block>((unsigned long)std::addressof(b->objects));
-				a->parent_marker = string_id(184552154);
-				a->child_object.TagGroup = blam_tag::tag_group_type::scenery;
-				a->child_object.TagIndex = tag_loader::ResolveNewDatum(0xE1BF0024);
+		if (!h2mod->Server) {
+			std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+			time_t tt = std::chrono::system_clock::to_time_t(now);
+			tm utc_tm = *gmtime(&tt);
+			if (utc_tm.tm_mon == 11 || utc_tm.tm_mon == 0) {
+				tag_loader::Load_tag(0xE19B001D, true, "christmas_hat_map");
+				tag_loader::Load_tag(0xE1BF0024, true, "christmas_hat_map");
+				tag_loader::Push_Back();
+				//auto scen = tags::get_tag<blam_tag::tag_group_type::scenery, s_scenery_group_definition>(datum(_INJECTED_TAG_START_));
+				auto hlmt_chief_datum = tags::find_tag(blam_tag::tag_group_type::model, "objects\\characters\\masterchief\\masterchief_mp");
+				if (hlmt_chief_datum != datum::Null) {
+					auto hlmt_chief = tags::get_tag<blam_tag::tag_group_type::model, s_model_group_definition>(hlmt_chief_datum);
+					auto b = hlmt_chief->variants[0];
+					auto a = MetaExtender::add_tag_block2<s_model_group_definition::s_variants_block::s_objects_block>((unsigned long)std::addressof(b->objects));
+					a->parent_marker = string_id(184552154);
+					a->child_object.TagGroup = blam_tag::tag_group_type::scenery;
+					a->child_object.TagIndex = tag_loader::ResolveNewDatum(0xE19B001D);
+				}
+				auto hlmt_elite_datum = tags::find_tag(blam_tag::tag_group_type::model, "objects\\characters\\elite\\elite_mp");
+				if (hlmt_elite_datum != datum::Null)
+				{
+					auto hlmt_eliete = tags::get_tag<blam_tag::tag_group_type::model, s_model_group_definition>(hlmt_elite_datum);
+					auto b = hlmt_eliete->variants[0];
+					auto a = MetaExtender::add_tag_block2<s_model_group_definition::s_variants_block::s_objects_block>((unsigned long)std::addressof(b->objects));
+					a->parent_marker = string_id(184552154);
+					a->child_object.TagGroup = blam_tag::tag_group_type::scenery;
+					a->child_object.TagIndex = tag_loader::ResolveNewDatum(0xE1BF0024);
+				}
 			}
 		}
 	}
