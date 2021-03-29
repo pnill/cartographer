ab8aa344ef4666c4028bc7f6100531ea82866b72
Kantanomo <mhatto00@gmail.com>
2021-03-29 11:50:13 -0500

    Thanks Himanshu.

--- xlive/H2MOD/Modules/Startup/Startup.cpp
+++ xlive/H2MOD/Modules/Startup/Startup.cpp
@@ -369,7 +369,6 @@ void InitH2Startup() {
 	}
 
 	H2BaseAddr = (DWORD)game_info.base;
-	Memory::setBaseAddress(H2BaseAddr, H2IsDediServer);
 
 	if (game_info.process_type == H2Types::H2Server)
 	{
@@ -381,6 +380,7 @@ void InitH2Startup() {
 		h2mod->Server = false;
 		H2IsDediServer = false;
 	}
+	Memory::setBaseAddress(H2BaseAddr, H2IsDediServer);
 
 	initInstanceNumber();
 
