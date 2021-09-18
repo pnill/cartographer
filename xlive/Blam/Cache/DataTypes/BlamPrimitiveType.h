02178d5b31878640fcac9751d667cd72c209640f
Unknown <cosminavieritei@gmail.com>
2021-09-18 22:31:37 +0300

    or'ing, not and'ing here, oops

--- xlive/Blam/Cache/DataTypes/BlamPrimitiveType.h
+++ xlive/Blam/Cache/DataTypes/BlamPrimitiveType.h
@@ -11,7 +11,7 @@ typedef int datum;
 
 #define DATUM_NONE NONE
 
-#define DATUM_NEW(_absolute_index, _salt) (datum)((_absolute_index) & ((_salt) << 16))
+#define DATUM_NEW(_absolute_index, _salt) (datum)((_absolute_index) | ((_salt) << 16))
 
 #define DATUM_IS_NONE(_absolute_index) ((int)(_absolute_index) == DATUM_NONE)
 #define DATUM_ABSOLUTE_INDEX(_absolute_index) (unsigned short)((_absolute_index) & 0xFFFF)
