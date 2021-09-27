03a4198aacf3830084e960471e63ab96cc3b2456
Unknown <cosminavieritei@gmail.com>
2021-09-27 10:42:12 +0300

    add other FLAG macros from BlamLib
    credits: https://github.com/HaloMods/OpenSauce

--- xlive/Blam/Common/Common.h
+++ xlive/Blam/Common/Common.h
@@ -1,9 +1,13 @@
 #pragma once
 
-//PADDING MACROS
+// Credits for some of the code:
+
+// https://github.com/HaloMods/OpenSauce
+
+// PADDING MACROS
 #define J( symbol1, symbol2 ) _DO_JOIN( symbol1, symbol2 )
 #define _DO_JOIN( symbol1, symbol2 ) symbol1##symbol2
-//Pad the desired length of BYTES and also hides it
+// Pad the desired length of BYTES and also hides it
 #define PAD(BYTES) private: BYTE J(Unused_, __LINE__ )[BYTES];  public:       
 
 /// Add an anonymous 8-bit (1 byte) field to a structure.
@@ -21,14 +25,21 @@
 /// Add an anonymous 128-bit (16 byte) field to a structure.
 #define PAD128 unsigned __int64 : 64; unsigned __int64 : 64;
 
-///Use this for setting up enum bitfields
+/// Use this for setting up enum bitfields
 #define FLAG(bit)( 1<<(bit) )
+#define TEST_FLAG(flags, bit)( ((flags) & FLAG(bit)) != 0 )
+#define SET_FLAG(flags, bit, value)( (value) ? ((flags) |= FLAG(bit)) : ((flags) &= ~FLAG(bit)) )
+#define SWAP_FLAG(flags, bit)			( (flags) ^=FLAG(bit) )
+#define FLAG_RANGE(first_bit, last_bit)	( (FLAG( (last_bit)+1 - (first_bit) )-1) << (first_bit) )
+
+/// Creates a mask out of a count number of flags
+#define MASK(count) ( (unsigned)(1 << (count)) - (unsigned)1 )
 
 #define CHECK_STRUCT_SIZE(STRUCT,_SIZE)\
-static_assert (sizeof(STRUCT) == _SIZE,"Invalid size for struct (" #STRUCT")")
+static_assert (sizeof(STRUCT) == _SIZE, "Invalid size for struct (" #STRUCT")")
 
 #define CHECK_STRUCT_OFFSET(STRUCT,FIELD,OFFSET)\
-static_assert(offsetof(STRUCT, FIELD) == OFFSET, #STRUCT " layout is invalid")
+static_assert (offsetof(STRUCT, FIELD) == OFFSET, #STRUCT " layout is invalid")
 
 #define TAG_BLOCK_SIZE_ASSERT(tagblock,size)\
 static_assert (sizeof(tagblock) == (size),"Invalid Size for TagBlock <" #tagblock ">")
