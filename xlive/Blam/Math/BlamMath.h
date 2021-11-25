9cb545734c9c085e6e0a1ae9f4fb8b8f7435ade5
NukeULater <nukeulater@gmail.com>
2021-11-25 21:58:38 +0200

    re-implement biped melee lunge physics function
    note: it still has some bugs/quirks to fix

--- xlive/Blam/Math/BlamMath.h
+++ xlive/Blam/Math/BlamMath.h
@@ -2,4 +2,5 @@
 
 #include "real_math.h"
 #include "integer_math.h"
+#include "real_vector3d.h"
 
--- xlive/H2MOD/Modules/GamePhysics/MeleeLunge.h
+++ xlive/H2MOD/Modules/GamePhysics/MeleeLunge.h
@@ -22,7 +22,7 @@ enum melee_flags
 #define melee_flags_mask 0xF0 // use first 4 bits of the 1 byte variable as flags
 #define melee_deceleration_tick_count_mask 0xFF000000
 
-struct character_physics_output
+struct s_character_physics_output
 {
 	DWORD flags;
 	BYTE gap_4[8];
@@ -31,7 +31,7 @@ struct character_physics_output
 	real_vector3d field_24;
 	BYTE gap_30[52];
 };
-CHECK_STRUCT_SIZE(character_physics_output, 0x64);
+CHECK_STRUCT_SIZE(s_character_physics_output, 0x64);
 
 struct __declspec(align(4)) c_character_physics_mode_melee_datum
 {
@@ -42,7 +42,7 @@ struct __declspec(align(4)) c_character_physics_mode_melee_datum
 	bool m_has_target;
 	BYTE field_E;
 	BYTE field_F;
-	real_point3d start_unit_origin;
+	real_point3d m_melee_start_origin;
 	real_vector3d field_1C;
 	float m_field_28;
 	float m_distance;
@@ -53,7 +53,7 @@ struct __declspec(align(4)) c_character_physics_mode_melee_datum
 	BYTE field_4D;
 	BYTE field_4E[2];
 	float m_velocity_to_decelerate;
-	float field_54;
+	float m_distance_to_target_point_before_deceleration;
 	real_vector3d field_58;
 	int field_64;
 	int field_68;
@@ -62,14 +62,21 @@ struct __declspec(align(4)) c_character_physics_mode_melee_datum
 	char gap_74[20];
 
 	bool pin_localized_velocity(real_vector3d* output, real_vector3d* localized_velocity);
-	void melee_deceleration_fixup(character_physics_output* output, real_point3d* object_origin, real_vector3d* current_velocity, real_vector3d* aiming_vector, BYTE& current_flags, BYTE& deceleration_tick_count);
+	void melee_deceleration_fixup(s_character_physics_output* output, real_point3d* object_origin, real_vector3d* current_velocity, real_vector3d* aiming_vector, BYTE& current_flags, BYTE& deceleration_tick_count);
 
-	void __thiscall update_internal(character_physics_output* a2, bool a3, float distance_world_units, real_vector3d *target_translational_velocity, real_point3d *target_origin, real_vector3d *aiming_vector, real_vector3d *translational_velocity, real_point3d *object_origin);
+	void __thiscall update_internal(s_character_physics_output* a2, bool a3, float distance_world_units, real_vector3d *target_translational_velocity, real_point3d *target_origin, real_vector3d *aiming_vector, real_vector3d *translational_velocity, real_point3d *object_origin);
+	void build_initial_melee_parameters(bool valid);
+	void update_melee_parameters();
+	void update_melee_deceleration();
+	void __thiscall update_internal_2(s_character_physics_output* a2, bool a3, float distance_world_units, real_vector3d* target_translational_velocity, real_point3d* target_origin, real_vector3d* aiming_vector, real_vector3d* translational_velocity, real_point3d* object_origin);
 };
 CHECK_STRUCT_SIZE(c_character_physics_mode_melee_datum, 0x88);
 
 void call_character_melee_physics_input_update_internal();
+void call_character_melee_physics_input_update_internal_2();
 
 void melee_force_decelerate_fixup();
 void melee_limit_some_random_vector();
 void __cdecl biped_dash_hook(datum object_index, datum target_player, char sword);
+
+extern bool melee_lunge_hook_enabled;
