0b900dc4832649b070fb70b3ede0c5a838a0167e
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-03-05 00:30:45 -0500

    Dont inline function in header

--- xlive/Blam/Engine/math/real_math.h
+++ xlive/Blam/Engine/math/real_math.h
@@ -179,7 +179,7 @@ struct real_plane3d
 };
 CHECK_STRUCT_SIZE(real_plane3d, sizeof(real_vector3d) + sizeof(float));
 
-float BLAM_MATH_INL plane3d_distance_to_point(const real_plane3d* plane, const real_point3d* point);
+float plane3d_distance_to_point(const real_plane3d* plane, const real_point3d* point);
 
 struct real_quaternion
 {
