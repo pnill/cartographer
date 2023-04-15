bc4e54b0f6346e5386418f8226d7cc452d28d51b
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-04-14 23:38:10 -0400

    update spacing in cheats header

--- xlive/Blam/Engine/game/cheats.h
+++ xlive/Blam/Engine/game/cheats.h
@@ -7,7 +7,7 @@
 //	Cow Bell was a planned skull for halo 2 and is using the string_id HS_UNLOCKED_EXTRA_15 (does not have a value set in the e_skull_type enum)
 //	Whuppopotamus is using the string_id HS_UNLOCKED_EXTRA_16 and is used in the game (value of 14 in the e_skull_type enum)
 //	In the original game the string id array for the skulls dosent actually include HS_UNLOCKED_EXTRA_16 in index 14 of the array 
-//  It's set to 0 (even on halo 2 xbox) so nothing displays when you pickup the skull, we fix this in the project
+//	It's set to 0 (even on halo 2 xbox) so nothing displays when you pickup the skull, we fix this in the project
 
 enum e_skull_type : unsigned int
 {
