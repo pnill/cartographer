f0035ea74bbb4089b76e6bc6b9e0744de7c9a6a1
nukeulater <nukeulater@gmail.com>
2023-05-15 17:16:03 +0300

    remove old high resolution workaround patches

--- xlive/H2MOD/Modules/Tweaks/Tweaks.h
+++ xlive/H2MOD/Modules/Tweaks/Tweaks.h
@@ -1,14 +1,5 @@
 #pragma once
 
-// TODO figure out where to move this
-struct ui_text_bounds
-{
-	short top;
-	short left;
-	short bottom;
-	short right;
-};
-
 namespace H2Tweaks {
 	void ApplyPatches();
 	void DisposePatches();
