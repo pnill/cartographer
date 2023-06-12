88ff6307894752be6c4ddba4519a1b05e4d910a6
nukeulater <nukeulater@gmail.com>
2023-06-12 09:21:37 +0300

    fix the custom exception handler not getting invoked

--- xlive/H2MOD/Modules/Shell/Debug/Debug.h
+++ xlive/H2MOD/Modules/Shell/Debug/Debug.h
@@ -5,5 +5,5 @@ namespace Debug
 	void Init();
 	void set_expection_filter(LPTOP_LEVEL_EXCEPTION_FILTER filter);
 	LPTOP_LEVEL_EXCEPTION_FILTER get_expection_filter();
-	LONG WINAPI On_UnhandledException(struct _EXCEPTION_POINTERS* ExceptionInfo);
+	LONG WINAPI UnhandledExceptionCb(struct _EXCEPTION_POINTERS* ExceptionInfo);
 }
