b7b9f0afb91e52a06381902444b85255e0156901
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-06-27 00:35:49 -0400

    add and update hs related enums and structs
    - remove hs_type_string map

---
+++ xlive/Blam/Engine/hs/hs_library_internal_runtime.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Blam/Cache/DataTypes/BlamPrimitiveType.h"
+#include "Blam/Common/Common.h"
+#include "hs.h"
+
+enum e_syntax_node_flags : WORD
+{
+	syntax_node_flag_primitive = FLAG(0),
+	syntax_node_flag_user_function = FLAG(1),
+	syntax_node_flag_global_reference = FLAG(2),
+	syntax_node_flag_bit_4 = FLAG(3),
+	syntax_node_flag_bit_5 = FLAG(4),
+	syntax_node_flag_bit_6 = FLAG(5),
+	syntax_node_flag_bit_7 = FLAG(6),
+	syntax_node_flag_bit_8 = FLAG(7),
+	syntax_node_flag_bit_9 = FLAG(8),
+	syntax_node_flag_bit_10 = FLAG(9),
+	syntax_node_flag_bit_11 = FLAG(10),
+	syntax_node_flag_bit_12 = FLAG(11),
+	syntax_node_flag_bit_13 = FLAG(12),
+	syntax_node_flag_bit_14 = FLAG(13),
+	syntax_node_flag_bit_15 = FLAG(14),
+	syntax_node_flag_bit_16 = FLAG(15)
+};
+// max count: k_maximum_hs_syntax_nodes_per_scenario 36864
+struct hs_syntax_node
+{
+    WORD salt;
+    short opcode;
+    e_hs_type type;
+	
+	e_syntax_node_flags expression_type;
+	datum next_expression;
+	int string_address;
+	char value_00_lsb;
+	char value_01;
+	char value_02;
+	char value_03_msb;
+};
+TAG_BLOCK_SIZE_ASSERT(hs_syntax_node, 20);
---
+++ xlive/Blam/Engine/hs/hs_unit_seats.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Blam/Cache/DataTypes/BlamPrimitiveType.h"
+#include "Blam/Common/Common.h"
+
+#define k_maximum_hs_unit_seat_mappings 65536
+
+enum e_seat_mapping_bitfield : int
+{
+	seat_mapping_bitfield_seat_0 = FLAG(0),
+	seat_mapping_bitfield_seat_1 = FLAG(1),
+	seat_mapping_bitfield_seat_2 = FLAG(2),
+	seat_mapping_bitfield_seat_3 = FLAG(3),
+	seat_mapping_bitfield_seat_4 = FLAG(4),
+	seat_mapping_bitfield_seat_5 = FLAG(5),
+	seat_mapping_bitfield_seat_6 = FLAG(6),
+	seat_mapping_bitfield_seat_7 = FLAG(7),
+	seat_mapping_bitfield_seat_8 = FLAG(8),
+	seat_mapping_bitfield_seat_9 = FLAG(9),
+	seat_mapping_bitfield_seat_10 = FLAG(10),
+	seat_mapping_bitfield_seat_11 = FLAG(11),
+	seat_mapping_bitfield_seat_12 = FLAG(12),
+	seat_mapping_bitfield_seat_13 = FLAG(13),
+	seat_mapping_bitfield_seat_14 = FLAG(14),
+	seat_mapping_bitfield_seat_15 = FLAG(15),
+	seat_mapping_bitfield_seat_16 = FLAG(16),
+	seat_mapping_bitfield_seat_17 = FLAG(17),
+	seat_mapping_bitfield_seat_18 = FLAG(18),
+	seat_mapping_bitfield_seat_19 = FLAG(19),
+	seat_mapping_bitfield_seat_20 = FLAG(20),
+	seat_mapping_bitfield_seat_21 = FLAG(21),
+	seat_mapping_bitfield_seat_22 = FLAG(22),
+	seat_mapping_bitfield_seat_23 = FLAG(23),
+	seat_mapping_bitfield_seat_24 = FLAG(24),
+	seat_mapping_bitfield_seat_25 = FLAG(25),
+	seat_mapping_bitfield_seat_26 = FLAG(26),
+	seat_mapping_bitfield_seat_27 = FLAG(27),
+	seat_mapping_bitfield_seat_28 = FLAG(28),
+	seat_mapping_bitfield_seat_29 = FLAG(29),
+	seat_mapping_bitfield_seat_30 = FLAG(30),
+	seat_mapping_bitfield_seat_31 = FLAG(31)
+};
+
+struct s_hs_unit_seat_mapping
+{
+	datum unit_tag_datum_index;
+	e_seat_mapping_bitfield seats;
+};
+TAG_BLOCK_SIZE_ASSERT(s_hs_unit_seat_mapping, 8);
