7331838dd1d1b6a3f3ad13fb4a105b38566a092f
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-06-27 13:05:52 -0400

    add scenario related game engine definitions

--- xlive/Blam/Engine/game/game_engine.h
+++ xlive/Blam/Engine/game/game_engine.h
@@ -1,7 +1,29 @@
 #pragma once
 
 #include "Blam/Common/Common.h"
-#pragma pack(push, 1)
+
+enum e_valid_multiplayer_games : short
+{
+	valid_multiplayer_game_capture_the_flag = FLAG(0),
+	valid_multiplayer_game_slayer = FLAG(1),
+	valid_multiplayer_game_oddball = FLAG(2),
+	valid_multiplayer_game_king_of_the_hill = FLAG(3),
+	valid_multiplayer_game_juggernaut = FLAG(4),
+	valid_multiplayer_game_territories = FLAG(5),
+	valid_multiplayer_game_assault = FLAG(6),
+};
+
+enum e_relevant_multiplayer_games : int
+{
+	relevant_multiplayer_game_capture_the_flag = FLAG(0),
+	relevant_multiplayer_game_slayer = FLAG(1),
+	relevant_multiplayer_game_oddball = FLAG(2),
+	relevant_multiplayer_game_king_of_the_hill = FLAG(3),
+	relevant_multiplayer_game_juggernaut = FLAG(4),
+	relevant_multiplayer_game_territories = FLAG(5),
+	relevant_multiplayer_game_assault = FLAG(6)
+};
+
 struct s_game_engine_globals
 {
 	byte gap0[10];
@@ -14,5 +36,4 @@ struct s_game_engine_globals
 
 	static s_game_engine_globals* get();
 };
-#pragma pack(pop)
 CHECK_STRUCT_SIZE(s_game_engine_globals, 0xCDC);
