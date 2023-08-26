f53b9bab03e110dec0cd75e02c94fed418ecf9c2
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-08-26 16:10:13 -0400

    fix incorrect typename

--- xlive/Blam/Engine/saved_games/game_variant.cpp
+++ xlive/Blam/Engine/saved_games/game_variant.cpp
@@ -3,8 +3,8 @@
 
 void game_variant_build_default(s_game_variant* variant, e_game_variant_description_index game_variant_type)
 {
-    typedef void(__stdcall* main_game_change_t)(s_game_variant*, e_game_variant_description_index game_variant_type);
-    auto p_game_variant_build_default = Memory::GetAddress<main_game_change_t>(0x5B33D, 0x3CF9D);
+    typedef void(__stdcall* game_variant_build_default_t)(s_game_variant*, e_game_variant_description_index game_variant_type);
+    auto p_game_variant_build_default = Memory::GetAddress<game_variant_build_default_t>(0x5B33D, 0x3CF9D);
     p_game_variant_build_default(variant, game_variant_type);
     return;
 }
