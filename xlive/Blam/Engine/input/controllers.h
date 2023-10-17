ea95973caf501881cade2591e4581613bd6340f8
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-10-17 17:24:06 -0400

    update e_controller_index

--- xlive/Blam/Engine/input/controllers.h
+++ xlive/Blam/Engine/input/controllers.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#define k_number_of_controllers 4
-
-enum e_controller_index : int
+enum e_controller_index : int32
 {
-	controller_index_0 = 0,
-	controller_index_1 = 1,
-	controller_index_2 = 2,
-	controller_index_3 = 3
+	_controller_index_0 = 0,
+	_controller_index_1 = 1,
+	_controller_index_2 = 2,
+	_controller_index_3 = 3,
+	k_number_of_controllers
 };
