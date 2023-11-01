8539581f9a1eed1dd4bb7bf5ea383a11dbb8e962
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-11-01 13:14:30 -0400

    add structure functions and definitions

--- xlive/Blam/Engine/structures/cluster_partitions.h
+++ xlive/Blam/Engine/structures/cluster_partitions.h
@@ -1,16 +1,11 @@
 #pragma once
-
 #include "Blam/Cache/DataTypes/TagRef.h"
 #include "Blam/Cache/DataTypes/TagBlock.h"
+#include "Blam/Engine/memory/data.h"
+#include "Blam/Engine/objects/object_placement.h"
 #include "Blam/Math/real_math.h"
 
 #define MAXIMUM_CLUSTERS_PER_STRUCTURE 512
-
-struct s_game_cluster_bit_vectors
-{
-    int32 cluster_bitvector;
-};
-CHECK_STRUCT_SIZE(s_game_cluster_bit_vectors, 4);
 
 // max count: MAXIMUM_CLUSTERS_PER_STRUCTURE 512
 struct s_scenario_cluster_property
@@ -32,3 +27,30 @@ struct s_scenario_cluster_data
     tag_block<s_scenario_cluster_property> atmospheric_fog_properties;
 };
 TAG_BLOCK_SIZE_ASSERT(s_scenario_cluster_data, 52);
+
+struct s_game_cluster_bit_vectors
+{
+    int32 cluster_bitvector;
+};
+CHECK_STRUCT_SIZE(s_game_cluster_bit_vectors, 4);
+
+struct cluster_partition
+{
+	int32* cluster_references;
+	s_data_array* array0;
+	s_data_array* array1;
+};
+
+void __cdecl cluster_partition_reconnect(cluster_partition* partition,
+	datum object_datum,
+	int32* first_cluster_reference,
+	const real_point3d* position,
+	real32 radius,
+	const s_location* location,
+	const s_game_cluster_bit_vectors* cluster_bitvector,
+	int32 payload_size,
+	const void* payload,
+	bool* cluster_overflow);
+
+cluster_partition* collideable_object_cluster_partition_get(void);
+cluster_partition* noncollideable_object_cluster_partition_get(void);
