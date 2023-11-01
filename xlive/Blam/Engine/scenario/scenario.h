b4be862d33496b166a03bd8b7921928bf3524612
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-11-01 13:56:06 -0400

    add scenario functions

--- xlive/Blam/Engine/scenario/scenario.h
+++ xlive/Blam/Engine/scenario/scenario.h
@@ -1,6 +1,15 @@
 #pragma once
 #include "scenario_definitions.h"
+#include "Blam/Engine/objects/object_placement.h"
 
 scenario* get_global_scenario(void);
 
+s_scenario_structure_bsp_group_definition::s_collision_bsp_block* global_collision_bsp_get(void);
+
 uint32 scenario_netgame_equipment_size(void);
+
+void location_invalidate(s_location* object_location);
+
+void __cdecl scenario_location_from_point(s_location* location, real_point3d* point);
+
+void __cdecl scenario_location_from_leaf(s_location* location, int32 leaf_index);
