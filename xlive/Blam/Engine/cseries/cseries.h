1dc3e4b45fd3ca89675c7ff50eaffc071a55cbff
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-11-17 22:58:49 -0500

    add static flags class
    - thanks twister

--- xlive/Blam/Engine/cseries/cseries.h
+++ xlive/Blam/Engine/cseries/cseries.h
@@ -47,6 +47,11 @@ static_assert(sizeof(real64) == 8);
 typedef long datum;
 static_assert(sizeof(datum) == 4);
 
+#define SIZEOF_BITS(value) 8 * sizeof(value)
+#define CHAR_BITS SIZEOF_BITS(int8)
+#define SHORT_BITS SIZEOF_BITS(int16)
+#define LONG_BITS SIZEOF_BITS(int32)
+
 // Invokes a function
 // ADDR_CLIENT: file offset in halo2.exe
 // ADDR_SERVER: file offset in h2server.exe
@@ -99,6 +104,11 @@ static_assert(sizeof(datum) == 4);
 #define SET_FLAG(flags, bit, value)( (value) ? ((flags) |= FLAG(bit)) : ((flags) &= ~FLAG(bit)) )
 #define SWAP_FLAG(flags, bit)			( (flags) ^=FLAG(bit) )
 #define FLAG_RANGE(first_bit, last_bit)	( (FLAG( (last_bit)+1 - (first_bit) )-1) << (first_bit) )
+
+#define BIT_VECTOR_SIZE_IN_LONGS(BIT_COUNT) (((BIT_COUNT) + (LONG_BITS - 1)) >> 5)
+#define BIT_VECTOR_SIZE_IN_BYTES(BIT_COUNT) (4 * BIT_VECTOR_SIZE_IN_LONGS(BIT_COUNT))
+#define BIT_VECTOR_TEST_FLAG(BIT_VECTOR, BIT) (TEST_BIT(BIT_VECTOR[BIT >> 5], (BIT & (LONG_BITS - 1))))
+#define BIT_VECTOR_SET_FLAG(BIT_VECTOR, BIT, ENABLE) (SET_FLAG(BIT_VECTOR[BIT >> 5], (BIT & (LONG_BITS - 1)), ENABLE))
 
 /// Creates a mask out of a count number of flags
 #define MASK(count) ( (unsigned)(1 << (count)) - (unsigned)1 )
