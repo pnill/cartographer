9ca2166003568ab88daee478fd1e46d39e5282f3
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-11-30 23:38:46 -0500

    misc changes

--- xlive/Blam/Engine/Networking/logic/life_cycle_manager.cpp
+++ xlive/Blam/Engine/Networking/logic/life_cycle_manager.cpp
@@ -3,12 +3,9 @@
 
 #include "Util/Hooks/Hook.h"
 
-e_game_life_cycle get_game_life_cycle()
+e_game_life_cycle __cdecl get_game_life_cycle(void)
 {
-	typedef e_game_life_cycle(__cdecl* get_lobby_state_t)();
-	auto p_get_game_life_cycle = Memory::GetAddress<get_lobby_state_t>(0x1AD660, 0x1A65DD);
-
-	return p_get_game_life_cycle();
+	return INVOKE(0x1AD660, 0x1A65DD, get_game_life_cycle);
 }
 
 void c_game_life_cycle_handler::initialize(void* life_cycle_manager, e_game_life_cycle life_cycle, bool unk_bool)
@@ -17,46 +14,61 @@ void c_game_life_cycle_handler::initialize(void* life_cycle_manager, e_game_life
 	this->life_cycle = life_cycle;
 	this->unk_bool = unk_bool;
 	static_cast<c_game_life_cycle_manager*>(this->life_cycle_manager)->life_cycle_handlers[this->life_cycle] = this;
+	return;
 }
 
-void __cdecl c_game_life_cycle_handler_joining::check_joining_capability()
+void __cdecl c_game_life_cycle_handler_joining::check_joining_capability(void)
 {
 	INVOKE(0x1AD643, 0x1A65C0, c_game_life_cycle_handler_joining::check_joining_capability);
 }
 
-bool c_game_life_cycle_manager::game_life_cycle_initialized()
+bool c_game_life_cycle_manager::game_life_cycle_initialized(void)
 {
 	return *Memory::GetAddress<bool*>(0x420FC0, 0x3C40A8);
 }
 
-c_game_life_cycle_manager* c_game_life_cycle_manager::get()
+c_game_life_cycle_manager* c_game_life_cycle_manager::get(void)
 {
 	return Memory::GetAddress<c_game_life_cycle_manager*>(0x420FC4, 0x3C40AC);
 }
 
-e_game_life_cycle c_game_life_cycle_manager::get_life_cycle() const
+e_game_life_cycle c_game_life_cycle_manager::get_life_cycle(void) const
 {
+	e_game_life_cycle result = _life_cycle_none;
 	if (game_life_cycle_initialized())
-		return life_cycle_state;
-	return _life_cycle_none;
+	{
+		result = life_cycle_state;
+	}
+	return result;
 }
 
-bool c_game_life_cycle_manager::state_is_joining() const
+bool c_game_life_cycle_manager::state_is_joining(void) const
 {
+	bool result = false;
 	if (!game_life_cycle_initialized())
-		return false;
-	if (life_cycle_state == _life_cycle_joining)
-		return true;
-	return false;
+	{
+		result = false;
+	}
+	else if (life_cycle_state == _life_cycle_joining)
+	{
+		result = true;
+	}
+
+	return result;
 }
 
-bool c_game_life_cycle_manager::state_is_in_game() const
+bool c_game_life_cycle_manager::state_is_in_game(void) const
 {
+	bool result = false;
 	if (!game_life_cycle_initialized())
-		return false;
-	if (life_cycle_state == _life_cycle_in_game)
-		return true;
-	return false;
+	{
+		result = false;
+	}
+	else if (life_cycle_state == _life_cycle_in_game)
+	{
+		result = true;
+	}
+	return result;
 }
 
 void c_game_life_cycle_manager::request_state_change(e_game_life_cycle requested_state, int unk_int, void* unk_ptr)
@@ -66,5 +78,9 @@ void c_game_life_cycle_manager::request_state_change(e_game_life_cycle requested
 	this->field_3C = unk_int;
 	this->field_40 = 0;
 	if (this->field_3C > 0)
+	{
 		memcpy(&field_40, unk_ptr, field_3C);
+	}
+
+	return;
 }
