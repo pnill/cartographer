d5345b1732e327bb1a6e24a8f2ba89d9a46c3697
Kantanomo <mhatto00@gmail.com>
2023-12-01 06:36:18 -0600

    remove unnecessary changes
    C++ 2023 not C 1981

--- xlive/Blam/Engine/Networking/logic/life_cycle_manager.cpp
+++ xlive/Blam/Engine/Networking/logic/life_cycle_manager.cpp
@@ -3,7 +3,7 @@
 
 #include "Util/Hooks/Hook.h"
 
-e_game_life_cycle __cdecl get_game_life_cycle(void)
+e_game_life_cycle __cdecl get_game_life_cycle()
 {
 	return INVOKE(0x1AD660, 0x1A65DD, get_game_life_cycle);
 }
@@ -14,25 +14,24 @@ void c_game_life_cycle_handler::initialize(void* life_cycle_manager, e_game_life
 	this->life_cycle = life_cycle;
 	this->unk_bool = unk_bool;
 	static_cast<c_game_life_cycle_manager*>(this->life_cycle_manager)->life_cycle_handlers[this->life_cycle] = this;
-	return;
 }
 
-void __cdecl c_game_life_cycle_handler_joining::check_joining_capability(void)
+void __cdecl c_game_life_cycle_handler_joining::check_joining_capability()
 {
 	INVOKE(0x1AD643, 0x1A65C0, c_game_life_cycle_handler_joining::check_joining_capability);
 }
 
-bool c_game_life_cycle_manager::game_life_cycle_initialized(void)
+bool c_game_life_cycle_manager::game_life_cycle_initialized()
 {
 	return *Memory::GetAddress<bool*>(0x420FC0, 0x3C40A8);
 }
 
-c_game_life_cycle_manager* c_game_life_cycle_manager::get(void)
+c_game_life_cycle_manager* c_game_life_cycle_manager::get()
 {
 	return Memory::GetAddress<c_game_life_cycle_manager*>(0x420FC4, 0x3C40AC);
 }
 
-e_game_life_cycle c_game_life_cycle_manager::get_life_cycle(void) const
+e_game_life_cycle c_game_life_cycle_manager::get_life_cycle() const
 {
 	e_game_life_cycle result = _life_cycle_none;
 	if (game_life_cycle_initialized())
@@ -42,33 +41,24 @@ e_game_life_cycle c_game_life_cycle_manager::get_life_cycle(void) const
 	return result;
 }
 
-bool c_game_life_cycle_manager::state_is_joining(void) const
+bool c_game_life_cycle_manager::state_is_joining() const
 {
-	bool result = false;
 	if (!game_life_cycle_initialized())
-	{
-		result = false;
-	}
-	else if (life_cycle_state == _life_cycle_joining)
-	{
-		result = true;
-	}
+		return false;
+	if (life_cycle_state == _life_cycle_joining)
+		return true;
 
-	return result;
+	return false;
 }
 
 bool c_game_life_cycle_manager::state_is_in_game(void) const
 {
-	bool result = false;
 	if (!game_life_cycle_initialized())
-	{
-		result = false;
-	}
-	else if (life_cycle_state == _life_cycle_in_game)
-	{
-		result = true;
-	}
-	return result;
+		return false;
+	if (life_cycle_state == _life_cycle_in_game)
+		return true;
+
+	return false;
 }
 
 void c_game_life_cycle_manager::request_state_change(e_game_life_cycle requested_state, int unk_int, void* unk_ptr)
@@ -81,6 +71,4 @@ void c_game_life_cycle_manager::request_state_change(e_game_life_cycle requested
 	{
 		memcpy(&field_40, unk_ptr, field_3C);
 	}
-
-	return;
 }
--- xlive/Blam/Engine/Networking/logic/life_cycle_manager.h
+++ xlive/Blam/Engine/Networking/logic/life_cycle_manager.h
@@ -41,7 +41,7 @@ struct c_game_life_cycle_handler_none : c_game_life_cycle_handler
 {
 	int8 gap[15];
 };
-CHECK_STRUCT_SIZE(c_game_life_cycle_handler_none, 32);
+CHECK_STRUCT_SIZE(c_game_life_cycle_handler_none, 0x20);
 
 struct c_game_life_cycle_handler_pre_game : c_game_life_cycle_handler
 {
@@ -50,7 +50,7 @@ struct c_game_life_cycle_handler_pre_game : c_game_life_cycle_handler
 	int32 field_18;
 	int8 gap_1C[15];
 };
-CHECK_STRUCT_SIZE(c_game_life_cycle_handler_pre_game, 44);
+CHECK_STRUCT_SIZE(c_game_life_cycle_handler_pre_game, 0x2C);
 
 struct c_game_life_cycle_handler_start_game : c_game_life_cycle_handler
 {
@@ -58,20 +58,20 @@ struct c_game_life_cycle_handler_start_game : c_game_life_cycle_handler
 	int8 field_11;
 	int8 gap_12[25];
 };
-CHECK_STRUCT_SIZE(c_game_life_cycle_handler_start_game, 44);
+CHECK_STRUCT_SIZE(c_game_life_cycle_handler_start_game, 0x2C);
 
 struct c_game_life_cycle_handler_in_game : c_game_life_cycle_handler
 {
 	int64 rand_bytes;
 	int8 gap_18[15];
 };
-CHECK_STRUCT_SIZE(c_game_life_cycle_handler_in_game, 40);
+CHECK_STRUCT_SIZE(c_game_life_cycle_handler_in_game, 0x28);
 
 struct c_game_life_cycle_handler_post_game : c_game_life_cycle_handler
 {
 	int8 gap[15];
 };
-CHECK_STRUCT_SIZE(c_game_life_cycle_handler_post_game, 32);
+CHECK_STRUCT_SIZE(c_game_life_cycle_handler_post_game, 0x20);
 
 class c_game_life_cycle_handler_joining : c_game_life_cycle_handler
 {
@@ -104,16 +104,16 @@ public:
 	int32 field_1078;
 	int8 gap_107C[4];
 	int32 join_attempt_result_code;
-	static void __cdecl check_joining_capability(void);
+	static void __cdecl check_joining_capability();
 };
-CHECK_STRUCT_SIZE(c_game_life_cycle_handler_joining, 4228);
+CHECK_STRUCT_SIZE(c_game_life_cycle_handler_joining, 0x1084);
 
 struct  c_game_life_cycle_handler_match_making : c_game_life_cycle_handler
 {
 	void* some_function_pointer;
 	int32 field_14;
 };
-CHECK_STRUCT_SIZE(c_game_life_cycle_handler_match_making, 24);
+CHECK_STRUCT_SIZE(c_game_life_cycle_handler_match_making, 0x18);
 
 class c_game_life_cycle_manager
 {
@@ -149,15 +149,14 @@ public:
 		this->update_requested = false;
 		this->requested_life_cycle = _life_cycle_none;
 		this->field_3C = 0;
-		this->field_40 = NULL;
+		this->field_40 = nullptr;
 	}
 
-	static bool game_life_cycle_initialized(void);
-	static c_game_life_cycle_manager* get(void);
-	e_game_life_cycle get_life_cycle(void) const;
-	bool state_is_joining(void) const;
-	bool state_is_in_game(void) const;
+	static bool game_life_cycle_initialized();
+	static c_game_life_cycle_manager* get();
+	e_game_life_cycle get_life_cycle() const;
+	bool state_is_joining() const;
+	bool state_is_in_game() const;
 	void request_state_change(e_game_life_cycle requested_state, int unk_int, void* unk_ptr);
-	
 };
-CHECK_STRUCT_SIZE(c_game_life_cycle_manager, 68);
+CHECK_STRUCT_SIZE(c_game_life_cycle_manager, 0x44);
