b0fa305c1c13f3b8119384e2befb98123add7a6d
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-12-05 20:18:25 -0500

    add ik_point_iterator class

---
+++ xlive/Blam/Engine/animations/ik_point_iterator.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Blam/Engine/tag_files/string_id.h"
+
+#define k_max_ik_points 8
+
+class ik_point_iterator
+{
+public:
+	ik_point_iterator();
+	~ik_point_iterator() = default;
+	void clear(void);
+
+	string_id marker;
+	string_id attach_to_marker;
+	int16 weapon_ik_index;
+	int16 field_A;
+	string_id field_C;
+	string_id field_10;
+};
