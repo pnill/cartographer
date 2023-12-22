c98e80d3231558ffb20cbdeb1f90cce522568ef3
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2023-12-22 15:35:23 -0500

    add comment

--- xlive/Blam/Engine/main/loading.cpp
+++ xlive/Blam/Engine/main/loading.cpp
@@ -5,6 +5,8 @@
 
 void loading_apply_patches(void)
 {
+	// Removes call to main_loading_idle to improve framerates
+	// Used for playing while disc installs (not supported in carto)
 	NopFill(Memory::GetAddress(0x39BAB), 5);
 	return;
 }
