d8d42cca624e1397be4e3bf5d2b98a84d69109f6
nukeulater <nukeulater@gmail.com>
2023-12-25 17:05:01 +0200

    fix static bit storage clearing, fixes weapon super-positions when interpolating

--- xlive/Blam/Engine/memory/static_arrays.h
+++ xlive/Blam/Engine/memory/static_arrays.h
@@ -4,12 +4,14 @@ template<typename t_type, typename t_storage_type, size_t k_count>
 class c_flags
 {
 public:
-	c_flags() : m_storage(0)
+	c_flags() 
+		: m_storage(0)
 	{
 		return;
 	}
 
-	c_flags(t_storage_type raw_bits) : m_storage(raw_bits)
+	c_flags(t_storage_type raw_bits) 
+		: m_storage(raw_bits)
 	{
 		return;
 	}
@@ -79,23 +81,26 @@ protected:
 	t_storage_type m_storage;
 };
 
-template<size_t k_maximum_count>
+template<size_t k_maximum_bit_count>
 class c_static_flags_no_init
 {
 public:
-	void* clear(void)
+	void clear(void)
 	{
-		return csmemset(this->m_flags, 0, NUMBEROF(m_flags));
+		csmemset(m_flags, 0, BIT_VECTOR_SIZE_IN_BYTES(k_maximum_bit_count));
+		return;
 	}
 
-	void* clear_range(int32 count)
+	void clear_range(int32 count)
 	{
-		return csmemset(this->m_flags, 0, BIT_VECTOR_SIZE_IN_BYTES(count));
+		csmemset(m_flags, 0, BIT_VECTOR_SIZE_IN_BYTES(count));
+		return;
 	}
 
-	void* fill(int32 count, uint8 value)
+	void fill(int32 count, uint8 value)
 	{
-		return csmemset(this->m_flags, value, BIT_VECTOR_SIZE_IN_BYTES(count));
+		csmemset(m_flags, value, BIT_VECTOR_SIZE_IN_BYTES(count));
+		return;
 	}
 
 	const int32* get_bits_direct(void) const
@@ -110,26 +115,27 @@ public:
 
 	void set(int32 index, bool enable)
 	{
-		BIT_VECTOR_SET_FLAG(this->m_flags, index, enable);
+		BIT_VECTOR_SET_FLAG(m_flags, index, enable);
 		return;
 	}
 
-	void* set_bits_direct_destructive(int32 count, uint8 value)
+	void set_bits_direct_destructive(int32 count, uint8 value)
 	{
-		return csmemcpy(this->m_flags, value, BIT_VECTOR_SIZE_IN_BYTES(count));
+		csmemcpy(m_flags, value, BIT_VECTOR_SIZE_IN_BYTES(count));
+		return;
 	}
 
 	bool test(int32 index)
 	{
-		return BIT_VECTOR_TEST_FLAG(this->m_flags, index);
+		return BIT_VECTOR_TEST_FLAG(m_flags, index);
 	}
 
 protected:
-	int32 m_flags[k_maximum_count / LONG_BITS];
+	int32 m_flags[k_maximum_bit_count / LONG_BITS];
 };
 
-template<size_t k_maximum_count>
-class c_static_flags : public c_static_flags_no_init<k_maximum_count>
+template<size_t k_maximum_bit_count>
+class c_static_flags : public c_static_flags_no_init<k_maximum_bit_count>
 {
 public:
 	c_static_flags(void) = default;
