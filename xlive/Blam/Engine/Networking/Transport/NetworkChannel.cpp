b138c2cf0389a7b045ea58eb54ed3415703c2510
nukeulater <nukeulater@gmail.com>
2024-02-10 10:01:45 +0200

    disable dedicated server game frame hook, misc minor updates

--- xlive/Blam/Engine/Networking/Transport/NetworkChannel.cpp
+++ xlive/Blam/Engine/Networking/Transport/NetworkChannel.cpp
@@ -2,18 +2,18 @@
 
 #include "NetworkChannel.h"
 
-s_network_channel* s_network_channel::get(int channelIdx)
+s_network_channel* s_network_channel::get(int32 channel_index)
 {
 	s_network_channel* network_channels = *Memory::GetAddress<s_network_channel**>(0x4FADBC, 0x525274);
-	return &network_channels[channelIdx];
+	return &network_channels[channel_index];
 }
 
-bool s_network_channel::get_network_address(network_address* out_addr)
+bool s_network_channel::get_network_address(network_address* address_out)
 {
 	bool result = false;
 	if (channel_state >= _channel_state_2)
 	{
-		memcpy(out_addr, &address, sizeof(network_address));
+		csmemcpy(address_out, &address, sizeof(network_address));
 		result = true;
 	}
 
