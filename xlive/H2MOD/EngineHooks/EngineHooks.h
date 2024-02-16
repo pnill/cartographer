fa276849e6de0cd8afeabc9b8e98e3cb628af8c8
nukeulater <nukeulater@gmail.com>
2024-02-16 13:12:41 +0200

    remove some redundant definitions

--- xlive/H2MOD/EngineHooks/EngineHooks.h
+++ xlive/H2MOD/EngineHooks/EngineHooks.h
@@ -4,9 +4,5 @@ namespace EngineHooks
 	typedef int(__thiscall* game_life_cycle_update)(BYTE* this_);
 	typedef void(__cdecl main_game_reset_map)();
 
-	typedef bool(__cdecl* verify_game_version_on_join)(BYTE executable_type, unsigned short executable_version, unsigned short compatible_version);
-	typedef bool(__cdecl* verify_executable_type)(BYTE executable_type);
-	typedef void(__cdecl* get_game_version)(BYTE* executable_type, DWORD* executable_version, DWORD* compatible_version);
-
 	void ApplyHooks();
 }
