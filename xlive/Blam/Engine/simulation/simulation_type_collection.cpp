505e5a72e0e957eb17cf834165a5894c9e62bfad
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2024-02-21 20:59:57 -0500

    add missing c_simulation_type_collection methods

--- xlive/Blam/Engine/simulation/simulation_type_collection.cpp
+++ xlive/Blam/Engine/simulation/simulation_type_collection.cpp
@@ -10,3 +10,15 @@ c_simulation_event_definition* c_simulation_type_collection::get_event_definitio
 {
 	return m_event_definitions[event_type];
 }
+
+void c_simulation_type_collection::register_entity_definition(e_simulation_entity_type entity_type, c_simulation_entity_definition* definition)
+{
+	m_entity_definitions[entity_type] = definition;
+	return;
+}
+
+void c_simulation_type_collection::register_event_definition(e_simulation_event_type type, c_simulation_event_definition* definition)
+{
+	m_event_definitions[type] = definition;
+	return;
+}
