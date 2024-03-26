4fc84efb3b2e835f9c9d023c0726233babde885f
nukeulater <nukeulater@gmail.com>
2024-03-26 20:53:14 +0200

    motion sensor sweep shader minor cleanup
    max is a standalone instruction in the shader model 2.0 spec already

--- xlive/Blam/Engine/rasterizer/dx9/shaders/motion_sensor_sweep.hlsl
+++ xlive/Blam/Engine/rasterizer/dx9/shaders/motion_sensor_sweep.hlsl
@@ -1,12 +1,7 @@
 // fxc /T ps_2_0 /Fo ../compiled/motion_sensor_sweep.pso ../motion_sensor_sweep.hlsl
 
-float hud_fade : register(c0);
+float1 hud_fade : register(c0);
 sampler2D s0 : register(s0);
-
-float max(float a, float b)
-{
-    return (a > b) ? a : b;
-}
 
 float4 main(float2 texcoord : TEXCOORD0, float4 color : COLOR0) : COLOR
 {
