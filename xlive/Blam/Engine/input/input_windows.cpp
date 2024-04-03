e540c90cdb7a26f68c18301b3d8b7a095f021819
Himanshu-01 <26570896+Himanshu-01@users.noreply.github.com>
2024-04-03 22:18:51 +0530

    Add `var_rumble_scale` command

--- xlive/Blam/Engine/input/input_windows.cpp
+++ xlive/Blam/Engine/input/input_windows.cpp
@@ -7,6 +7,7 @@
 #include "interface/user_interface_controller.h"
 
 s_vibration_state g_vibration_state[k_number_of_controllers]{};
+float g_rumble_factor = 1.0f;
 
 int32* hs_debug_simulate_gamepad_global_get(void)
 {
@@ -20,19 +21,22 @@ bool* input_suppress_global_get(void)
 
 void __cdecl input_set_gamepad_rumbler_state(int16 gamepad_index, uint16 left, uint16 right)
 {
-    ASSERT(VALID_INDEX(gamepad_index, k_number_of_controllers));
+	ASSERT(VALID_INDEX(gamepad_index, k_number_of_controllers));
 
-    s_vibration_state state = { left, right };
-    s_vibration_state state_none = { 0, 0 };
+	s_vibration_state state = { left, right };
+	s_vibration_state state_none = { 0, 0 };
 
-    bool enabled = user_interface_controller_get_rumble_enabled((e_controller_index)gamepad_index);
-    g_vibration_state[gamepad_index] = (enabled ? state : state_none);
-    return;
+	state.left = PIN(state.left * g_rumble_factor, 0, UINT16_MAX);
+	state.right = PIN(state.right * g_rumble_factor, 0, UINT16_MAX);
+
+	bool enabled = user_interface_controller_get_rumble_enabled((e_controller_index)gamepad_index);
+	g_vibration_state[gamepad_index] = (enabled ? state : state_none);
+	return;
 }
 
 
 void input_windows_apply_patches(void)
 {
-    PatchCall(Memory::GetAddress(0x9020F), input_set_gamepad_rumbler_state);    // Replace call in rumble_clear_all_now
-    return;
+	PatchCall(Memory::GetAddress(0x9020F), input_set_gamepad_rumbler_state);    // Replace call in rumble_clear_all_now
+	return;
 }
