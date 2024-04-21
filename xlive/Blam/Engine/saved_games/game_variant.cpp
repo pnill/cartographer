17f5563869c8931505b1c30fee0675cf5bd32088
nukeulater <nukeulater@gmail.com>
2024-04-21 11:04:44 +0300

    fixed game variant build call convention

--- xlive/Blam/Engine/saved_games/game_variant.cpp
+++ xlive/Blam/Engine/saved_games/game_variant.cpp
@@ -10,7 +10,7 @@ s_game_variant* get_game_variant(void)
 
 void game_variant_build_default(s_game_variant* variant, e_game_variant_description_index game_variant_type)
 {
-    typedef void(__stdcall* game_variant_build_default_t)(s_game_variant*, e_game_variant_description_index game_variant_type);
+    typedef void(__cdecl* game_variant_build_default_t)(s_game_variant*, e_game_variant_description_index game_variant_type);
     auto p_game_variant_build_default = Memory::GetAddress<game_variant_build_default_t>(0x5B33D, 0x3CF9D);
     p_game_variant_build_default(variant, game_variant_type);
     return;
