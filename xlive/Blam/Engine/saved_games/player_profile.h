6cd95f2073187ad1faa7de1c09603249721ac004
Himanshu-01 <26570896+Himanshu-01@users.noreply.github.com>
2024-05-15 23:24:08 +0530

    Added `s_saved_game_file_player_profile`

---
+++ xlive/Blam/Engine/saved_games/player_profile.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Blam/Engine/cseries/cseries.h"
+#include "Blam/Engine/game/players.h"
+
+#define k_default_profiles_count 1
+
+
+
+#pragma pack(push,1)
+struct s_saved_game_controller_settings
+{
+	PAD(3660);
+};
+ASSERT_STRUCT_SIZE(s_saved_game_controller_settings, 3660);
+
+struct s_saved_game_file_player_profile
+{
+	uint32 field_0;
+	uint32 field_4;
+	c_static_wchar_string32 player_name;
+	c_static_string<16> gamertag;
+	PAD(4);
+	uint32 m_last_level_map_id;
+	PAD(136);
+	uint8 search_data[112];
+	c_static_wchar_string128 search_description;
+	PAD(148);
+	s_saved_game_controller_settings controller_settings;
+	PAD(16);
+	s_player_profile_traits profile;
+	PAD(176);
+};
+ASSERT_STRUCT_SIZE(s_saved_game_file_player_profile, 4616);
+#pragma pack(pop)
