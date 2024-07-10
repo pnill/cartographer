640b5539973bcc9e6101ec18369e3f8791409174
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2024-07-10 17:02:34 -0400

    adjust weapon offset string code
    - use safe string functions and use static strings

--- xlive/H2MOD/Modules/WeaponOffsets/WeaponOffsetConfig.cpp
+++ xlive/H2MOD/Modules/WeaponOffsets/WeaponOffsetConfig.cpp
@@ -1,55 +1,55 @@
 #include "stdafx.h"
-
 #include "WeaponOffsetConfig.h"
 
-static std::wstring GetOffsetPath()
-{
-	std::wstring offsetPath;
-	offsetPath = _wgetenv(L"localappdata");
-	offsetPath += L"\\Microsoft\\Halo 2\\WeaponOffsets.cfg";
-	return offsetPath;
-}
+#include "cseries/cseries_strings.h"
+
+/* prototypes */
+void weapon_offset_get_path(c_static_wchar_string<MAX_PATH>* path);
+
+/* public code */
 
 void ReadWeaponOffsetConfig(s_weapon_custom_offset* weaponOffsets, int count)
 {
-	FILE* file;
-	std::wstring path = GetOffsetPath();
+	c_static_wchar_string<MAX_PATH> path;
+	weapon_offset_get_path(&path);
 
-	file = _wfopen(path.c_str(), L"r");
+	FILE* file = _wfopen(path.get_string(), L"r");
 	if (file != NULL)
 	{
 		for (int i = 0; i < count; i++)
 		{
-			fscanf(file, "%f,%f,%f\n", &weaponOffsets[i].modifiedOffset.i, &weaponOffsets[i].modifiedOffset.j, &weaponOffsets[i].modifiedOffset.k);
+			fscanf_s(file, "%f,%f,%f\n", &weaponOffsets[i].modifiedOffset.i, &weaponOffsets[i].modifiedOffset.j, &weaponOffsets[i].modifiedOffset.k);
 		}
 		fclose(file);
 	}
+	return;
 }
 
 void SaveWeaponOffsetConfig(const s_weapon_custom_offset* customOffsets, int count, bool defaultOffsets)
 {
-	FILE* file;
-	std::wstring path = GetOffsetPath();
+	c_static_wchar_string<MAX_PATH> path;
+	weapon_offset_get_path(&path);
 
-	file = _wfopen(path.c_str(), L"w");
+	FILE* file = _wfopen(path.get_string(), L"w");
 	if (file != NULL)
 	{
 		for (int i = 0; i < count; i++)
 		{
 			const real_vector3d& offset = defaultOffsets ? customOffsets[i].defaultOffset : customOffsets[i].modifiedOffset;
-			fprintf(file, "%.3f,%.3f,%.3f\n", offset.i, offset.j, offset.k);
+			fprintf_s(file, "%.3f,%.3f,%.3f\n", offset.i, offset.j, offset.k);
 		}
 		fclose(file);
 	}
+	return;
 }
 
 // only writes anything if file dosent already exist
 void WriteDefaultFile(const s_weapon_custom_offset* weaponOffsets, int count)
 {
-	FILE* file = NULL;
-	std::wstring path = GetOffsetPath();
+	c_static_wchar_string<MAX_PATH> path;
+	weapon_offset_get_path(&path);
 
-	file = _wfopen(path.c_str(), L"r");
+	FILE* file = _wfopen(path.get_string(), L"r");
 	if (file == NULL)
 	{
 		SaveWeaponOffsetConfig(weaponOffsets, count, true);
@@ -58,4 +58,14 @@ void WriteDefaultFile(const s_weapon_custom_offset* weaponOffsets, int count)
 	{
 		fclose(file);
 	}
+	return;
+}
+
+/* private code */
+
+void weapon_offset_get_path(c_static_wchar_string<MAX_PATH>* path)
+{
+	path->set(_wgetenv(L"localappdata"));
+	path->append(L"\\Microsoft\\Halo 2\\WeaponOffsets.cfg");
+	return;
 }
