aa5c711f8340cd278d33080c6c5c3860e1fdba1f
Kantanomo <804386+Kantanomo@users.noreply.github.com>
2024-07-12 13:34:16 -0500

    update signature to return correct data-type

--- xlive/Blam/Engine/cache/pc_texture_cache.h
+++ xlive/Blam/Engine/cache/pc_texture_cache.h
@@ -2,6 +2,8 @@
 #include "bitmaps/bitmap_group.h"
 #include "memory/data.h"
 
+#define k_max_pc_texture_cache_count 10000
+
 // todo: unfinished very large struct
 struct s_pc_texture_cache_globals
 {
@@ -10,4 +12,4 @@ struct s_pc_texture_cache_globals
 
 IDirect3DTexture9* __cdecl texture_cache_bitmap_get_hardware_format_add_to_texture_cache(bitmap_data* data, real32 a2, int32 a3);
 
-bool __cdecl pc_texture_cache_preload_bitmap(bitmap_data* bitmap_data, uint8 flags, real32 unk, bool* unused);
+IDirect3DTexture9* __cdecl pc_texture_cache_preload_bitmap(bitmap_data* bitmap_data, uint8 flags, real32 unk, bool* unused);
