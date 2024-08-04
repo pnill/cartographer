539d1cd1cb69198e674e9257be7060768867d953
Kantanomo <804386+Kantanomo@users.noreply.github.com>
2024-08-04 14:47:41 -0500

    Macro fun
    reworked logic of asserts to work in release build

--- xlive/Blam/Engine/filesys/pc_file_system.cpp
+++ xlive/Blam/Engine/filesys/pc_file_system.cpp
@@ -3,6 +3,8 @@
 
 void file_seek_and_read(FILE* file_handle, uint32 file_offset, uint32 read_size, uint32 read_count, void* out_buffer)
 {
-	ASSERT(fseek(file_handle, file_offset, SEEK_SET) == 0);
-	ASSERT(fread(out_buffer, read_size, read_count, file_handle));
+	bool seek_result = fseek(file_handle, file_offset, SEEK_SET) == 0;
+	ASSERT(seek_result);
+	bool read_result = fread(out_buffer, read_size, read_count, file_handle);
+	ASSERT(read_result);
 }
