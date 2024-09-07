de098f0a21a2fccbe141cbdab0e3d7f3a59f0710
Kantanomo <804386+Kantanomo@users.noreply.github.com>
2024-09-07 09:58:22 -0500

    more zombies work
    fixed all  sections of zombies that strictly referenced local user 0
    
    refactored and re-did hud_draw_player_indicators, now can control the player indicator rendering per-user
    
    added helper function to player globals to get controller_index from user index.

--- xlive/Blam/Engine/interface/new_hud_draw.h
+++ xlive/Blam/Engine/interface/new_hud_draw.h
@@ -15,6 +15,10 @@ datum hud_bitmap_tag_index_get(void);
 
 int32 hud_bitmap_data_index_get(void);
 
+void hud_player_indicators_draw_enabled_set(int32 user_index, bool enabled);
+
+void hud_player_indicators_draw_reset();
+
 void __cdecl draw_hud_layer(void);
 
 void new_hud_draw_apply_patches();
