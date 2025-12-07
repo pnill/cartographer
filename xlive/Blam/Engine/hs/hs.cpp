0c26a7adc08c6608752b491a38ad144040767bda
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2025-12-07 17:08:24 -0500

    fix internal global logic
    - don't set the final bit in the global index to one if internal

--- xlive/Blam/Engine/hs/hs.cpp
+++ xlive/Blam/Engine/hs/hs.cpp
@@ -806,7 +806,7 @@ int16 hs_find_global_by_name(const char* name)
 				const hs_global_internal* global = assert_return(HS_GLOBAL_INTERNAL_GET(global_index));
 				if (!csstricmp(name, global->name))
 				{
-					result = HS_GLOBAL_INDEX(global_index) | FLAG(SHORT_BITS - 1);
+					result = HS_GLOBAL_INDEX(global_index);
 					global_found = true;
 					break;
 				}
