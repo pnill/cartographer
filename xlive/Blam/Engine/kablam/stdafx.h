fed40b68628e8e159efa168f26ddbe931b341182
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2025-12-19 18:58:42 -0500

    set windows 2000 as the minimum windows version

--- xlive/Blam/Engine/kablam/stdafx.h
+++ xlive/Blam/Engine/kablam/stdafx.h
@@ -6,6 +6,10 @@
 
 #pragma comment(lib, "Rpcrt4.lib")
 
+// Windows 2000 is the current min requirement
+#define WINVER 0x0500
+#define _WIN32_WINNT 0x0500
+
 #include <assert.h>
 #include <Windows.h>
 
