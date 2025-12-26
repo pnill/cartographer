7562cd82cd1747f07ef371a5250f81dff1197d21
nukeulater <18323679+nukeulater@users.noreply.github.com>
2025-12-26 09:48:40 +0200

    replace hacky extra param patch for edge drop fix

--- xlive/Blam/Engine/physics/character_physics_mode_ground.cpp
+++ xlive/Blam/Engine/physics/character_physics_mode_ground.cpp
@@ -17,25 +17,17 @@ static void __stdcall c_character_physics_mode_ground_datum__update_internal_hoo
 // fixes the biped unit movement physics from applying too much movement, especially when edge-dropping by adjusting the default constant (0.117) value to tickrate
 __declspec(naked) static void c_character_physics_mode_ground_datum__update_internal_update_constant(void)
 {
-#define _stack_pointer_offset 4h + 4Ch
-#define _last_param_offset 4h + 10h
-	__asm
-	{
-		movss xmm2, [esp + _stack_pointer_offset + _last_param_offset]
-		ret
-	}
-#undef _stack_pointer_offset
-#undef _last_param_offset
-}
+	static const real32 k_edge_drop_value = 0.117f * 30.f;
 
-__declspec(naked) static void c_character_physics_mode_ground_datum__update_internal_to_stdcall(void)
-{
 	__asm
 	{
-		pop eax // pop return address
-		push ecx // push ecx as first param
-		push eax // push the return address back on stack
-		jmp c_character_physics_mode_ground_datum__update_internal_hook
+		sub esp, 4
+		call game_tick_length
+		fstp [esp]
+		movss xmm2, [esp]
+		mulss xmm2, [k_edge_drop_value]
+		add esp, 4
+		ret
 	}
 }
 
@@ -44,22 +36,18 @@ __declspec(naked) static void c_character_physics_mode_ground_datum__update_inte
 void character_physics_mode_ground_apply_patches(void)
 {
 	// fixes edge drop fast fall when using higher tickrates than 30
-	PatchCall(Memory::GetAddress(0x1082B4, 0xFA5D4), c_character_physics_mode_ground_datum__update_internal_to_stdcall);
 	Codecave(Memory::GetAddress(0x106E23, 0xF9143), c_character_physics_mode_ground_datum__update_internal_update_constant, 3);
 	return;
 }
 
 
-static void __stdcall c_character_physics_mode_ground_datum__update_internal_hook(
+static void __stdcall c_character_physics_mode_ground_datum__update_internal(
 	class c_character_physics_mode_ground_datum* thisx,
 	struct s_character_physics_update_output_datum* physics_output,
 	const struct s_character_physics_update_input_datum* physics_input,
 	bool localize_airborne,
 	const real_vector3d* localized_velocity)
 {
-	const real32 k_edge_drop_value = 0.117f;
-	const real32 edge_drop_per_tick = 30.f * k_edge_drop_value * game_tick_length();
-
 	// push last parameter despite the function taking just 5 parameters
 	INVOKE_TYPE(
 		0x1067F0,
@@ -69,19 +57,14 @@ static void __stdcall c_character_physics_mode_ground_datum__update_internal_hoo
 			struct s_character_physics_update_output_datum*,
 			const struct s_character_physics_update_input_datum*,
 			bool,
-			const real_vector3d*,
-			real32
+			const real_vector3d*
 		),
 		thisx,
 		physics_output,
 		physics_input,
 		localize_airborne,
-		localized_velocity,
-		edge_drop_per_tick
+		localized_velocity
 	);
 
-
-	// account for the last parameter that doesn't get handled by the actual function
-	__asm add esp, 4;
 	return;
 }
