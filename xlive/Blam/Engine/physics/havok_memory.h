2dae7027c23b9610323f22d678e69bb916b706f8
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2026-01-12 08:55:57 -0500

    add havok memory tracking structs

--- xlive/Blam/Engine/physics/havok_memory.h
+++ xlive/Blam/Engine/physics/havok_memory.h
@@ -1,3 +1,62 @@
 #pragma once
 
+/* structures */
+
+struct s_runtime_pool_memory_usage
+{
+	struct s_bucket_stats
+	{
+		int32 bucket_size;
+		int32 total_elements;
+		int32 elements_in_use;
+	};
+
+	int32 runtime_memory;
+	int32 total_pages;
+	int32 pages_in_use;
+	int32 page_size;
+	int32 current_page_free_bytes;
+	real32 memory_usage_fraction;
+	s_bucket_stats buckets[16];
+	int32 debug_memory_size;
+	int32 bucket_count;
+	int8 gap[64];
+};
+
+struct s_startup_pool_memory_usage
+{
+	int32 memory_total;
+	int32 memory_in_use;
+	int32 allocation_count;
+	int32 debug_memory_size;
+};
+
+struct s_overflow_pool_memory_usage
+{
+	int32 memory_total;
+	int32 memory_in_use;
+	int32 allocation_count;
+	int32 debug_memory_size;
+};
+
+struct s_2nd_overflow_pool_memory_usage
+{
+	int32 memory_total;
+	int32 memory_in_use;
+	int32 maximum_allocations;
+	int32 allocation_count;
+	int32 debug_memory_size;
+};
+
+struct s_havok_memory_usage
+{
+	s_runtime_pool_memory_usage runtime_pool_memory_usage;
+	s_startup_pool_memory_usage startup_pool_memory_usage;
+	s_overflow_pool_memory_usage overflow_pool_memory_usage;
+	s_2nd_overflow_pool_memory_usage second_overflow_pool_memory_usage;
+};
+ASSERT_STRUCT_SIZE(s_havok_memory_usage, 340);
+
+/* prototypes */
+
 void __cdecl havok_memory_garbage_collect(void);
