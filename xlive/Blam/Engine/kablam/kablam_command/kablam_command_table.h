9bd6c5a93b19f984e9f4ed4b1d8523597b5deaf6
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2026-08-18 22:32:05 -0400

    setup llvm builds correctly for h2admin

--- xlive/Blam/Engine/kablam/kablam_command/kablam_command_table.h
+++ xlive/Blam/Engine/kablam/kablam_command/kablam_command_table.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "kablam_command.h"
 
 /* constants */
 
@@ -21,7 +22,7 @@ public:
 /* prototypes */
 
 class kablam_command* kablam_command_table_create_command(wchar_t** arguments, uint32 argument_count, bool print_messages, uint32* out_command_table_index);
-void kablam_command_table_print_help_function(enum e_kablam_command_type command_type);
+void kablam_command_table_print_help_function(e_kablam_command_type command_type);
 
 void kablam_command_table_parse_input(wchar_t* command_buffer, wchar_t** command_arguments, uint32 max_argument_count, uint32* command_argument_count);
 void kablam_command_table_parse_response(class kablam_command* command, uint32 command_table_index);
