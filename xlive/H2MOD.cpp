8ba93437c8d4d37647eef116cd72e25b5d78945c
nukeulater <18323679+nukeulater@users.noreply.github.com>
2026-09-14 02:46:11 +0300

    get max grenade count from globals

--- xlive/H2MOD.cpp
+++ xlive/H2MOD.cpp
@@ -370,17 +370,22 @@ static bool __cdecl OnPlayerSpawn(datum player_index)
 		unit_datum* unit = unit_get(player->unit_index);
 
 		ASSERT(player);
-		ASSERT(unit);
-		
-		if (variant->cartographer_settings.flags.test(_cartographer_variant_invincible_players))
-		{
-			unit->object.object_damage_flags.set(_object_is_immune_to_damage, true);
-		}
 
-		if (variant->cartographer_settings.flags.test(_cartographer_variant_infinite_grenades))
+		if (unit)
 		{
-			unit->unit.grenade_counts[_unit_grenade_human_fragmentation] = 4;
-			unit->unit.grenade_counts[_unit_grenade_covenant_plasma] = 4;
+			if (variant->cartographer_settings.flags.test(_cartographer_variant_invincible_players))
+			{
+				unit->object.object_damage_flags.set(_object_is_immune_to_damage, true);
+			}
+
+			if (variant->cartographer_settings.flags.test(_cartographer_variant_infinite_grenades))
+			{
+				s_game_globals_grenade const* frag_grenade_globals = TAG_BLOCK_GET_ELEMENT(&scenario_get_game_globals()->grenades, _unit_grenade_human_fragmentation, s_game_globals_grenade);
+				s_game_globals_grenade const* plasma_grenade_globals = TAG_BLOCK_GET_ELEMENT(&scenario_get_game_globals()->grenades, _unit_grenade_covenant_plasma, s_game_globals_grenade);
+
+				unit->unit.grenade_counts[_unit_grenade_human_fragmentation] = (int8)frag_grenade_globals->maximum_count;
+				unit->unit.grenade_counts[_unit_grenade_covenant_plasma] = (int8)plasma_grenade_globals->maximum_count;
+			}
 		}
 	}
 
