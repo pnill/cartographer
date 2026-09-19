e07367189c7b2b3fa41c8203ae856cdad5c72991
Kantanomo <804386+Kantanomo@users.noreply.github.com>
2026-09-19 12:01:49 -0500

    another debug assert fix for gungame

--- xlive/H2MOD/Variants/GunGame/GunGame.cpp
+++ xlive/H2MOD/Variants/GunGame/GunGame.cpp
@@ -188,7 +188,10 @@ void GunGame::OnPlayerSpawn(ExecTime execTime, datum player_index)
 
 bool GunGame::c_game_statborg__adjust_player_stat(ExecTime execTime, c_game_statborg* statborg, datum player_index, e_statborg_entry statistic, short count, int game_results_statistic, bool adjust_team_stat)
 {
-	player_datum const* player = player_get(player_index);
+	player_datum const* player = player_try_and_get_absolute(player_index);
+
+	if (!player)
+		return false;
 
 	uint64 id;
 	s_player_identifier identifier = NetworkSession::GetPlayerId(player_index);
